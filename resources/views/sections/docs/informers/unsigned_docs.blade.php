493768630f185056a169703158a8aa3a8be24981
Paul A. Pleshkov <pleshkovpa@yandex.ru>
2023-11-08 16:34:30 +0300

    sc

--- resources/views/sections/docs/informers/unsigned_docs.blade.php
+++ resources/views/sections/docs/informers/unsigned_docs.blade.php
@@ -2,7 +2,7 @@
     <div class="my-3 p-3 bg-body rounded shadow-sm" style="max-width: 12rem;">
         <div class="card-header">Документов к ознакомлению</div>
         <div class="card-body text-primary">
-            <a href="{{ route('osfrportal.docs.index') }}"><h1 class="bi bi-file-medical card-text"> 123 </h1></a>
+            <a class="text-decoration-none" href="{{ route('osfrportal.docs.index') }}"><h1 class="bi bi-file-medical card-text"> 123 </h1></a>
         </div>
     </div>
 </div>
