9ac5fbeafe2cbd3246601cece8f52e3de61c6733
Paul A. Pleshkov <pleshkovpa@yandex.ru>
2023-11-08 17:47:38 +0300

    sc

---
+++ src/Actions/Docs/CountUnsignedDocsByUserAction.php
@@ -0,0 +1,27 @@
+<?php
+namespace Osfrportal\OsfrportalLaravel\Actions\Docs;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+
+use Osfrportal\OsfrportalLaravel\Models\SfrDocs;
+
+class CountUnsignedDocsByUserAction
+{
+    use AsAction;
+
+    public function handle()
+    {
+        $allDocs = SfrDocs::where('doc_data->docNeedSign', true)->with(['SfrDocsFiles', 'SfrDocsUserSigns'])->get();
+        $collectionDocs = collect();
+        $collectionSigns = collect();
+        foreach ($allDocs as $doc) {
+            foreach ($doc->SfrDocsUserSigns as $s) {
+                $collectionSigns->push([
+                    'sign_fileid' => $s['sign_fileid'],
+                    'sign_pid' => $s['sign_pid']
+                ]);
+            }
+        }
+        $collectionSigns->dump();
+    }
+}
