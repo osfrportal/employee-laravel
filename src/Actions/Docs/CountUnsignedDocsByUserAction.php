201163f537de0e2c84a29c373182bded6d9a431c
Paul A. Pleshkov <pleshkovpa@yandex.ru>
2023-11-08 17:53:54 +0300

    sc

--- src/Actions/Docs/CountUnsignedDocsByUserAction.php
+++ src/Actions/Docs/CountUnsignedDocsByUserAction.php
@@ -16,7 +16,7 @@ class CountUnsignedDocsByUserAction
         $collectionSigns = collect();
         
         foreach ($allDocs as $doc) {
-            $doc->SfrDocsFiles->dump();
+            $doc->dump();
             foreach ($doc->SfrDocsUserSigns as $s) {
                 $collectionSigns->push([
                     'sign_fileid' => $s['sign_fileid'],
