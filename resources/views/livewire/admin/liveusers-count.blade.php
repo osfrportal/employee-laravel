799b54d1815c36871606b43630f0c6ad4c6222b2
Paul A. Pleshkov <pleshkovpa@yandex.ru>
2023-11-08 17:09:54 +0300

    sc

--- resources/views/livewire/admin/liveusers-count.blade.php
+++ resources/views/livewire/admin/liveusers-count.blade.php
@@ -1,6 +1,6 @@
 <div>
-    <div class="my-3 p-3 bg-body rounded shadow-sm" style="max-width: 12rem;" wire:poll.5s>
-        <div class="card-header text-center">Активные пользователи</div>
+    <div class="my-3 p-3 bg-body rounded shadow-sm text-center" style="max-width: 12rem;" wire:poll.5s>
+        <div class="card-header">Активные пользователи</div>
         <div class="card-body text-primary">
             <h2 class="bi bi-people card-text"> {{ number_format($this->liveUsersCount, 0) }}</h2>
         </div>
--- resources/views/sections/docs/informers/unsigned_docs.blade.php
+++ resources/views/sections/docs/informers/unsigned_docs.blade.php
@@ -1,5 +1,5 @@
-    <div class="my-3 p-3 bg-body rounded shadow-sm" style="max-width: 15rem;">
-        <div class="card-header text-center">Документов к ознакомлению</div>
+    <div class="my-3 p-3 bg-body rounded shadow-sm text-center" style="max-width: 15rem;">
+        <div class="card-header">Документов к ознакомлению</div>
         <div class="card-body text-primary">
             <a class="text-decoration-none" href="{{ route('osfrportal.docs.index') }}"><h2 class="bi bi-file-medical card-text"> 123 </h2></a>
         </div>
