798ad304e9d7a674489060a79ec4cb982a9f6712
Paul A. Pleshkov <pleshkovpa@yandex.ru>
2023-11-08 16:44:29 +0300

    sc

--- resources/views/sections/dashboard/dashboard.blade.php
+++ resources/views/sections/dashboard/dashboard.blade.php
@@ -2,8 +2,18 @@
 @section('dashboardTitle', 'Личный кабинет')
 @section('content')
     @can('admin-menu-show')
-    @livewire('osfrportal::liveusers-count')
-    @include('osfrportal::sections.docs.informers.unsigned_docs')
+    <div class="container">
+        <div class="row">
+          <div class="col-3">
+            @livewire('osfrportal::liveusers-count')
+          </div>
+          <div class="col-3">
+            @include('osfrportal::sections.docs.informers.unsigned_docs')
+          </div>
+        </div>
+      </div>
+
+
     @endcan
 
     @include('osfrportal::sections.dashboard.notifications.notifications_unread')
