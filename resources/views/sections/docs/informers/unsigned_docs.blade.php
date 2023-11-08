de5d4358a0aacf108ff9cb98b97cddd7005034e9
Paul A. Pleshkov <pleshkovpa@yandex.ru>
2023-11-08 18:33:14 +0300

    sc

--- resources/views/sections/docs/informers/unsigned_docs.blade.php
+++ resources/views/sections/docs/informers/unsigned_docs.blade.php
@@ -1,5 +1,5 @@
     <div class="my-3 p-3 bg-body rounded shadow-sm text-center" style="max-width: 15rem;">
-        <div class="card-header">Документов к ознакомлению</div>
+        <div class="card-header">Документов на ознакомление</div>
         <div class="card-body text-primary">
             <a class="text-decoration-none" href="{{ route('osfrportal.docs.index') }}"><h2 class="bi bi-file-medical card-text"> {{ $docsUnsignedCount ?? '0' }} </h2></a>
         </div>
