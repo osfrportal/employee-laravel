a03af040dadaba2bcc3727111092928f6e91a968
Paul A. Pleshkov <pleshkovpa@yandex.ru>
2023-11-15 23:17:44 +0300

    l

--- resources/views/admin/logs/logsphoneupdates.blade.php
+++ resources/views/admin/logs/logsphoneupdates.blade.php
@@ -26,13 +26,27 @@
                 <tr>
                     <th scope="row">{{ $logRecord['created_at'] }}</th>
                     <td>{{ $logRecord['message'] }}</td>
-                    <td><a href="{{ route('osfrportal.admin.persons.detail', ['personid' => $logRecord['personPid']]) }}">{{ $logRecord['personFullName'] }}</a></td>
-                    <td>
-                        @foreach ($logRecord['differences'] as $diffkey => $diffvalue)
-                           {{ $logKeysDescription[$diffkey] ?? ''}}: {{ $diffvalue['old'] ?? '' }} => {{ $diffvalue['new'] ?? '' }}
-                        @endforeach
+                    <td><a
+                            href="{{ route('osfrportal.admin.persons.detail', ['personid' => $logRecord['personPid']]) }}">{{ $logRecord['personFullName'] }}</a>
                     </td>
-                    <td><a href="{{ route('osfrportal.admin.persons.detail', ['personid' => $logRecord['sfrperson_pid']]) }}">{{ $logRecord['sfrperson_fio'] }} ({{ $logRecord['sfrperson_username'] }})</a></td>
+                    <td>
+                        <table class="table table-sm table-responsive table-striped">
+                            @foreach ($logRecord['differences'] as $diffkey => $diffvalue)
+                                <tr>
+                                    <td colspan="3">
+                                        {{ $logKeysDescription[$diffkey] ?? '' }}
+                                    </td>
+                                    <td>{{ $diffvalue['old'] ?? '' }}</td>
+                                    <td> => </td>
+                                    <td>{{ $diffvalue['new'] ?? '' }}</td>
+
+                                </tr>
+                            @endforeach
+                        </table>
+                    </td>
+                    <td><a
+                            href="{{ route('osfrportal.admin.persons.detail', ['personid' => $logRecord['sfrperson_pid']]) }}">{{ $logRecord['sfrperson_fio'] }}
+                            ({{ $logRecord['sfrperson_username'] }})</a></td>
                 </tr>
             @endforeach
         </tbody>
