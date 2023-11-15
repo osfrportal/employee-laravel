6330e2cfee742d531553de22272aacdfade21179
Paul A. Pleshkov <pleshkovpa@yandex.ru>
2023-11-15 23:43:16 +0300

    l

--- resources/views/admin/logs/logsphoneupdates.blade.php
+++ resources/views/admin/logs/logsphoneupdates.blade.php
@@ -23,35 +23,38 @@
         </thead>
         <tbody>
             @foreach ($phoneLogs as $logRecord)
-                <tr>
-                    <th scope="row">{{ $logRecord['created_at'] }}</th>
-                    <td>{{ $logRecord['message'] }}</td>
-                    <td><a
-                            href="{{ route('osfrportal.admin.persons.detail', ['personid' => $logRecord['personPid']]) }}">{{ $logRecord['personFullName'] }}</a>
-                    </td>
-                    <td class="text-center">
-                        @if (empty($logRecord['differences']))
-                            Данные не изменялись
-                        @else
-                        <table class="table table-sm mb-0 align-middle">
-                            @foreach ($logRecord['differences'] as $diffkey => $diffvalue)
-                                <tr>
-                                    <td>
-                                        {{ $logKeysDescription[$diffkey] ?? '' }}
-                                    </td>
-                                    <td>{{ $diffvalue['old'] ?? '' }}</td>
-                                    <td><i class="bi bi-arrow-right"></i></td>
-                                    <td>{{ $diffvalue['new'] ?? '' }}</td>
+                @if (!empty($logRecord['differences']))
+                    <tr>
+                        <th scope="row">{{ $logRecord['created_at'] }}</th>
+                        <td>{{ $logRecord['message'] }}</td>
+                        <td><a
+                                href="{{ route('osfrportal.admin.persons.detail', ['personid' => $logRecord['personPid']]) }}">{{ $logRecord['personFullName'] }}</a>
+                        </td>
+                        <td class="text-center">
+                            @if (empty($logRecord['differences']))
+                                Данные не изменялись
+                            @else
+                                <table class="table table-sm mb-0 align-middle">
+                                    @foreach ($logRecord['differences'] as $diffkey => $diffvalue)
+                                        <tr>
+                                            <td>
+                                                {{ $logKeysDescription[$diffkey] ?? '' }}
+                                            </td>
+                                            <td>{{ $diffvalue['old'] ?? '' }}</td>
+                                            <td><i class="bi bi-arrow-right"></i></td>
+                                            <td>{{ $diffvalue['new'] ?? '' }}</td>
 
-                                </tr>
-                            @endforeach
-                        </table>
-                        @endif
-                    </td>
-                    <td><a
-                            href="{{ route('osfrportal.admin.persons.detail', ['personid' => $logRecord['sfrperson_pid']]) }}">{{ $logRecord['sfrperson_fio'] }}
-                            ({{ $logRecord['sfrperson_username'] }})</a></td>
-                </tr>
+                                        </tr>
+                                    @endforeach
+                                </table>
+                            @endif
+                        </td>
+                        <td><a
+                                href="{{ route('osfrportal.admin.persons.detail', ['personid' => $logRecord['sfrperson_pid']]) }}">{{ $logRecord['sfrperson_fio'] }}
+                                ({{ $logRecord['sfrperson_username'] }})
+                            </a></td>
+                    </tr>
+                @endif
             @endforeach
         </tbody>
     </table>
