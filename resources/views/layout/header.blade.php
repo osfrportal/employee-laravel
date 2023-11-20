3e06000f9aaccdd9be2997c4e5a87afd5ac458e7
osfrportal <pleshkovpa@48.sfr.gov.ru>
2023-11-20 11:03:56 +0300

    some changes

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -3,7 +3,7 @@
     <meta name="description" content="">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" type="image/ico" href="{{ asset('osfrportal/images/favicon.ico') }}" />
-
+    <link href="{{ asset('osfrportal/css/sfrfonts.css') }}" rel="stylesheet">
     <link
         href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
         rel="stylesheet">
