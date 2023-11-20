cb21a65f972342654ca68d568f5b160c48e0d3e4
Paul A. Pleshkov <pleshkovpa@yandex.ru>
2023-11-20 21:25:22 +0300

    sc

--- resources/views/livewire/notifications-count.blade.php
+++ resources/views/livewire/notifications-count.blade.php
@@ -1,12 +1,11 @@
 <div wire:poll.5s>
     <svg xmlns="http://www.w3.org/2000/svg"
-                class="icon icon-tabler icon-tabler-bell icon-tada iconsMenu" width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                class="icon icon-tabler icon-tabler-bell iconsMenu @if ($unreadNotificationsCount > 0) icon-tada @endif" width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
                 stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                 <path d="M10 5a2 2 0 1 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6"></path>
                 <path d="M9 17v1a3 3 0 0 0 6 0v-1"></path>
             </svg>
-    <i class="bi bi-bell iconsMenu"></i>
     @if ($unreadNotificationsCount > 0)
         <span class="position-absolute start-100 translate-middle badge rounded-pill bg-danger">
             {{ $unreadNotificationsCount }}
