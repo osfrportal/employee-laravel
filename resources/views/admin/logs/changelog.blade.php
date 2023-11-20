13b04d6eb40a8cdaa7981ac0419fcdf33b22bb08
Paul A. Pleshkov <pleshkovpa@yandex.ru>
2023-11-20 20:07:59 +0300

    sc

--- resources/views/admin/logs/changelog.blade.php
+++ resources/views/admin/logs/changelog.blade.php
@@ -138,7 +138,7 @@
                         <path d="M18 19a2 2 0 0 0 2 -2v-4l1 -1l-1 -1v-4a2 2 0 0 0 -2 -2" />
                       </svg> Добавлены уведомления о синхронизации. (уведомления пользователям
                         группы system-notifications)</li>
-                    <li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-code-plus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-code-plus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" title="[ADD]">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                         <path d="M9 12h6" />
                         <path d="M12 9v6" />
