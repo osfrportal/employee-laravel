e0343d63d898110ed2a8fdc3f78f591a1a439a29
Paul A. Pleshkov <pleshkovpa@yandex.ru>
2023-11-20 20:00:34 +0300

    sc

--- resources/views/admin/logs/changelog.blade.php
+++ resources/views/admin/logs/changelog.blade.php
@@ -23,8 +23,8 @@
             </svg>
             <div class="pb-3 mb-0  lh-sm border-bottom w-100">
                 <span class="d-block  mb-2">20-11-2023</span>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">[FIX] <i class="ti ti-code-asterix" width="48" height="48"></i> Портал переведен на использование корпоративного шрифта Montserrat</li>
+                <ul class="list-group list-group-flush small">
+                    <li class="list-group-item">[FIX] <i class="ti ti-code-asterix"></i> Портал переведен на использование корпоративного шрифта Montserrat</li>
                     <li class="list-group-item">[FIX] <i class="ti ti-code-asterix"></i> Скрипты и стили перенесены на внутренний ресурс для ускорения загрузки</li>
                 </ul>
             </div>
@@ -55,8 +55,20 @@
             <div class="pb-3 mb-0  lh-sm border-bottom w-100">
                 <span class="d-block  mb-2">11-11-2023</span>
                 <ul class="list-group list-group-flush small ">
-                    <li class="list-group-item">[ADD] <i class="ti ti-code-plus"></i> Добавлен столбец Группа документов в управление документами</li>
-                    <li class="list-group-item">[ADD] <i class="ti ti-code-plus"></i> Добавлено журналирование в БД</li>
+                    <li class="list-group-item">[ADD] <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-code-plus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M9 12h6" />
+                        <path d="M12 9v6" />
+                        <path d="M6 19a2 2 0 0 1 -2 -2v-4l-1 -1l1 -1v-4a2 2 0 0 1 2 -2" />
+                        <path d="M18 19a2 2 0 0 0 2 -2v-4l1 -1l-1 -1v-4a2 2 0 0 0 -2 -2" />
+                      </svg> Добавлен столбец Группа документов в управление документами</li>
+                    <li class="list-group-item">[ADD] <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-code-plus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M9 12h6" />
+                        <path d="M12 9v6" />
+                        <path d="M6 19a2 2 0 0 1 -2 -2v-4l-1 -1l1 -1v-4a2 2 0 0 1 2 -2" />
+                        <path d="M18 19a2 2 0 0 0 2 -2v-4l1 -1l-1 -1v-4a2 2 0 0 0 -2 -2" />
+                      </svg> Добавлено журналирование в БД</li>
                 </ul>
             </div>
         </div>
