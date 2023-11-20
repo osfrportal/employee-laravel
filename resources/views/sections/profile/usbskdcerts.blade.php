fde05c384bac83fc3dd434a29e5a7c88c9e0ccb9
Paul A. Pleshkov <pleshkovpa@yandex.ru>
2023-11-20 21:36:56 +0300

    sc

--- resources/views/sections/profile/usbskdcerts.blade.php
+++ resources/views/sections/profile/usbskdcerts.blade.php
@@ -119,6 +119,22 @@
                                 <div
                                     class="d-flex align-items-center justify-content-between px-4 @if ($cert->revoked) {{ 'alert alert-danger' }} @else {{ $cert->certvalidto->isPast() ? 'alert alert-dark' : 'alert alert-success' }} @endif">
                                     <div class="d-flex align-items-center">
+                                        @if ($cert->revoked || $cert->certvalidto->isPast())
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-award-off" width="32" height="32" viewBox="0 0 32 32" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                            <path d="M16.72 12.704a6 6 0 0 0 -8.433 -8.418m-1.755 2.24a6 6 0 0 0 7.936 7.944" />
+                                            <path d="M12 15l3.4 5.89l1.598 -3.233l.707 .046m1.108 -2.902l-1.617 -2.8" />
+                                            <path d="M6.802 12l-3.4 5.89l3.598 -.233l1.598 3.232l3.4 -5.889" />
+                                            <path d="M3 3l18 18" />
+                                          </svg>
+                                        @else
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-award" width="32" height="32" viewBox="0 0 32 32" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                            <path d="M12 9m-6 0a6 6 0 1 0 12 0a6 6 0 1 0 -12 0" />
+                                            <path d="M12 15l3.4 5.89l1.598 -3.233l3.598 .232l-3.4 -5.889" />
+                                            <path d="M6.802 12l-3.4 5.89l3.598 -.233l1.598 3.232l3.4 -5.889" />
+                                          </svg>
+                                        @endif
                                         <img src="{{ asset('osfrportal/images/logo_certificate.svg') }}" alt=""
                                             class="icon-small" />
                                         <div class="ms-4">
