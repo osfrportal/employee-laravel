34323b6f8313c91c3475ef8216b3dedb2247fdb4
osfrportal <pleshkovpa@48.sfr.gov.ru>
2023-11-24 18:57:09 +0300

    some changes

---
+++ src/Data/SFRUnitData.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Osfrportal\OsfrportalLaravel\Data;
+
+use Spatie\LaravelData\Data;
+use Spatie\LaravelData\DataCollection;
+use Spatie\LaravelData\Attributes\DataCollectionOf;
+
+class SFRUnitData extends Data
+{
+    public function __construct(
+        //make each property of the data object nullable like this:
+        //public ?string $room,
+        public ?string $unitid = null,
+        public ?string $unitname = null,
+        public ?string $unitnameshort = null,
+        public ?string $unitcode = null,
+        public ?string $unitparentid = null,
+        public ?string $unitsortorder = null,
+        #[DataCollectionOf(SFRUnitData::class)]
+        public ?DataCollection $childunits = null,
+    ) {}
+
+    public static function defValues(): SFRUnitData {
+        return new self(
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null
+        );
+    }
+}
