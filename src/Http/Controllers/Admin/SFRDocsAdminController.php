bbf63ec9498f0c2d50c9a73baedf443456a49023
Paul A. Pleshkov <pleshkovpa@yandex.ru>
2023-11-24 23:40:17 +0300

    sc

--- src/Http/Controllers/Admin/SFRDocsAdminController.php
+++ src/Http/Controllers/Admin/SFRDocsAdminController.php
@@ -241,13 +241,27 @@ class SFRDocsAdminController extends Controller
     {
         $allRootUnits = SfrUnits::whereNull('unitparentid')->orderBy('unitsortorder', 'ASC')->with('children')->orderBy('unitname', 'ASC')->get();
         foreach ($allRootUnits as $rootUnit) {
-            $unitData = ['unitid' => $rootUnit->unitid, 'unitname' => $rootUnit->unitname, 'unitcode' => $rootUnit->unitcode, 'unitnameshort' => $rootUnit->unitnameshort, 'unitparentid' => $rootUnit->unitparentid, 'unitsortorder' => $rootUnit->unitsortorder];
+            $unitData = [
+                'unitid' => $rootUnit->unitid, 
+                'unitname' => $rootUnit->unitname, 
+                'unitcode' => $rootUnit->unitcode, 
+                'unitnameshort' => $rootUnit->unitnameshort, 
+                'unitparentid' => $rootUnit->unitparentid, 
+                'unitsortorder' => $rootUnit->unitsortorder
+            ];
             $childUnits = [];
             if (count($rootUnit->children) > 0) {
                 foreach ($rootUnit->children as $childUnit) {
-                    $childUnits[] = ['unitid' => $childUnit->unitid, 'unitname' => $childUnit->unitname, 'unitcode' => $childUnit->unitcode, 'unitnameshort' => $childUnit->unitnameshort, 'unitparentid' => $childUnit->unitparentid, 'unitsortorder' => $childUnit->unitsortorder];
+                    $childUnits[] = [
+                        'unitid' => $childUnit->unitid, 
+                        'unitname' => $childUnit->unitname, 
+                        'unitcode' => $childUnit->unitcode, 
+                        'unitnameshort' => $childUnit->unitnameshort, 
+                        'unitparentid' => $childUnit->unitparentid, 
+                        'unitsortorder' => $childUnit->unitsortorder
+                    ];
                 }
-                $unitData = Arr::prepend($unitData, 'childunits', SFRUnitData::collection($childUnits));
+                $unitData = Arr::prepend($unitData, SFRUnitData::collection($childUnits), 'childunits');
             }
             dump(SFRUnitData::from($unitData));
         }
