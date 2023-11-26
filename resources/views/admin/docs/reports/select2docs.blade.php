3c7dc0b94e5751924b08c64d60e2ef5f25de078c
Paul A. Pleshkov <pleshkovpa@yandex.ru>
2023-11-26 17:38:51 +0300

    sc

--- resources/views/admin/docs/reports/select2docs.blade.php
+++ resources/views/admin/docs/reports/select2docs.blade.php
@@ -1,16 +1,25 @@
 <select class="form-select form-select-sm mb-3" id="js-all-sfrdocs-ajax" name="sfrdocs[]"
-    data-placeholder="Выберите документы" data-minimum-input-length="0"
-    data-selection-css-class="select2--small" data-dropdown-css-class="select2--small" multiple="multiple"></select>
+    data-placeholder="Выберите документы" data-minimum-input-length="0" data-selection-css-class="select2--small"
+    data-dropdown-css-class="select2--small" multiple="multiple"></select>
 
 @push('footer-scripts')
     <script type="module">
         $(document).ready(function() {
+            var test = $.getJSON("{{ route('osfrapi.osfrportal.admin.select2.docs.allgrouped') }}", function(data) {
+                console.log(data.text);
+                console.log(data.error);
+            });
+            console.log(test);
             $('#js-all-sfrdocs-ajax').select2({
                 ajax: {
                     dataType: 'json',
                     delay: 500,
                     url: "{{ route('osfrapi.osfrportal.admin.select2.docs.allgrouped') }}",
                 }
+            });
+            $('#js-all-sfrdocs-ajax').on('select2:opening select2:closing', function(event) {
+                var $searchfield = $(this).parent().find('.select2-search__field');
+                $searchfield.prop('disabled', true);
             });
         });
     </script>
--- resources/views/admin/docs/reports/select2units.blade.php
+++ resources/views/admin/docs/reports/select2units.blade.php
@@ -5,47 +5,16 @@
 @push('footer-scripts')
     <script type="module">
         $(document).ready(function() {
-            function matchStart(params, data) {
-                // If there are no search terms, return all of the data
-                if ($.trim(params.term) === '') {
-                    return data;
-                }
-
-                // Skip if there is no 'children' property
-                if (typeof data.children === 'undefined') {
-                    return null;
-                }
-
-                // `data.children` contains the actual options that we are matching against
-                var filteredChildren = [];
-                $.each(data.children, function(idx, child) {
-                    if (child.text.toUpperCase().indexOf(params.term.toUpperCase()) == 0) {
-                        filteredChildren.push(child);
-                    }
-                });
-
-                // If we matched any of the timezone group's children, then set the matched children on the group
-                // and return the group object
-                if (filteredChildren.length) {
-                    var modifiedData = $.extend({}, data, true);
-                    modifiedData.children = filteredChildren;
-
-                    // You can return modified objects from here
-                    // This includes matching the `children` how you want in nested data sets
-                    return modifiedData;
-                }
-
-                // Return `null` if the term should not be displayed
-                return null;
-            }
-
             $('#js-all-sfrunits-ajax').select2({
-                matcher: matchStart,
                 ajax: {
                     dataType: 'json',
                     delay: 500,
                     url: "{{ route('osfrapi.osfrportal.admin.select2.units.all') }}",
                 }
+            });
+            $('#js-all-sfrunits-ajax').on('select2:opening select2:closing', function(event) {
+                var $searchfield = $(this).parent().find('.select2-search__field');
+                $searchfield.prop('disabled', true);
             });
         });
     </script>
