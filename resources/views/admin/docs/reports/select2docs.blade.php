68266b2f3fbe726891744ace446484e32a48855c
Paul A. Pleshkov <pleshkovpa@yandex.ru>
2023-11-26 18:07:11 +0300

    sc

--- resources/views/admin/docs/reports/select2docs.blade.php
+++ resources/views/admin/docs/reports/select2docs.blade.php
@@ -5,26 +5,15 @@
 @push('footer-scripts')
     <script type="module">
         $(document).ready(function() {
-            var context;
             $.ajax({
                 url: "{{ route('osfrapi.osfrportal.admin.select2.docs.allgrouped') }}",
                 async: false,
                 dataType: 'json',
                 success: function(json) {
-                    assignVariable(json);
+                    $('#js-all-sfrdocs-ajax').select2({
+                        data: json
+                    });
                 }
-            });
-
-            function assignVariable(data) {
-                context = data;
-            }
-            console.log(context);
-            $('#js-all-sfrdocs-ajax').select2({
-                data: context
-            });
-            $('#js-all-sfrdocs-ajax').on('select2:opening select2:closing', function(event) {
-                var $searchfield = $(this).parent().find('.select2-search__field');
-                $searchfield.prop('disabled', true);
             });
         });
     </script>
