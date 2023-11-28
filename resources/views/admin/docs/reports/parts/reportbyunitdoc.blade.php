6a343d21871367e00e4c62fcff7414a9be6f63cb
osfrportal <pleshkovpa@48.sfr.gov.ru>
2023-11-28 18:17:40 +0300

    some changes

---
+++ resources/views/admin/docs/reports/parts/reportbyunitdoc.blade.php
@@ -0,0 +1,18 @@
+@extends('osfrportal::layout')
+@section('title2')
+    <div class="pt-0">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="#">Администрирование</a></li>
+                <li class="breadcrumb-item">Управление документами</li>
+                <li class="breadcrumb-item">Отчеты</li>
+                <li class="breadcrumb-item active">Ознакомление по подразделениям</li>
+            </ol>
+        </nav>
+    </div>
+@endsection
+@section('content')
+    @foreach ($SFRDocData as $doc)
+        @dump($doc)
+    @endforeach
+@endsection
