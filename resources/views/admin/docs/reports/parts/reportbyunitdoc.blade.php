d762f4f14c2ef637fbae522324f20fe3ad248c09
osfrportal <pleshkovpa@48.sfr.gov.ru>
2023-11-28 19:07:04 +0300

    some changes

--- resources/views/admin/docs/reports/parts/reportbyunitdoc.blade.php
+++ resources/views/admin/docs/reports/parts/reportbyunitdoc.blade.php
@@ -24,44 +24,51 @@
         <tbody class="table-group-divider">
             @foreach ($allDocsArray as $doc)
                 @foreach ($doc->docPersonSigns as $person)
-                @dump($person->signData->count())
-                    @foreach ($person->signData as $personSign)
+                    @if ($person->signData->count() > 0)
+                        @foreach ($person->signData as $personSign)
+                            <tr>
+                                <td>{{ $doc->docTypeName }} {{ $doc->docName }}</td>
+                                <td>{{ $person->personData->persondata_fullname }}</td>
+                                <td>{{ $personSign->signDateTime ?? '' }}</td>
+                                <td>
+                                    <div class="stamp">
+                                        <div class="d-flex justify-content-start stampmain">
+                                            <div class="p2"><img src="{{ asset('osfrportal/images/logo.svg') }}"
+                                                    alt=""></div>
+                                            <div class="p2">&nbsp;</div>
+                                            <div class="align-self-center p2">Документ подписан электронной подписью
+                                                ({{ $personSign->signLabel ?? '' }})
+                                            </div>
+                                        </div>
+                                        <table class="stampbottom">
+                                            <tr>
+                                                <td>Сертификат:</td>
+                                                <td>{{ $personSign->signCertHash ?? '' }}</td>
+                                            </tr>
+                                            <tr class="stampbold">
+                                                <td>Владелец:</td>
+                                                <td>{{ $personSign->signCertCN ?? '' }}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Действителен:</td>
+                                                <td>{{ $personSign->signCertValidDates ?? '' }}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Выдан:</td>
+                                                <td>{{ $personSign->signIssuerCN ?? '' }}</td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    @else
                         <tr>
                             <td>{{ $doc->docTypeName }} {{ $doc->docName }}</td>
                             <td>{{ $person->personData->persondata_fullname }}</td>
-                            <td>{{ $personSign->signDateTime ?? '' }}</td>
-                            <td>
-                                <div class="stamp">
-                                    <div class="d-flex justify-content-start stampmain">
-                                        <div class="p2"><img src="{{ asset('osfrportal/images/logo.svg') }}"
-                                                alt=""></div>
-                                        <div class="p2">&nbsp;</div>
-                                        <div class="align-self-center p2">Документ подписан электронной подписью
-                                            ({{ $personSign->signLabel ?? '' }})
-                                        </div>
-                                    </div>
-                                    <table class="stampbottom">
-                                        <tr>
-                                            <td>Сертификат:</td>
-                                            <td>{{ $personSign->signCertHash ?? '' }}</td>
-                                        </tr>
-                                        <tr class="stampbold">
-                                            <td>Владелец:</td>
-                                            <td>{{ $personSign->signCertCN ?? '' }}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Действителен:</td>
-                                            <td>{{ $personSign->signCertValidDates ?? '' }}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Выдан:</td>
-                                            <td>{{ $personSign->signIssuerCN ?? '' }}</td>
-                                        </tr>
-                                    </table>
-                                </div>
-                            </td>
+                            <td colspan="2">Не ознакомлен</td>
                         </tr>
-                    @endforeach
+                    @endif
                 @endforeach
             @endforeach
         </tbody>
