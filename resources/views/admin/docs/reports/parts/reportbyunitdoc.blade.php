f0660291b0582720b5e35661d4dd3614115a513a
osfrportal <pleshkovpa@48.sfr.gov.ru>
2023-11-28 19:19:54 +0300

    some changes

--- resources/views/admin/docs/reports/parts/reportbyunitdoc.blade.php
+++ resources/views/admin/docs/reports/parts/reportbyunitdoc.blade.php
@@ -12,70 +12,82 @@
     </div>
 @endsection
 @section('content')
-    <table class="table table-hover table-responsive">
-        <thead>
-            <tr class="align-middle">
-                <th scope="col">Документ</th>
-                <th scope="col">Подразделение</th>
-                <th scope="col">Работник</th>
-                <th scope="col">Дата ознакомления</th>
-                <th scope="col">Подпись</th>
-            </tr>
-        </thead>
-        <tbody class="table-group-divider">
-            @foreach ($allDocsArray as $doc)
-                @foreach ($doc->docPersonSigns as $person)
-                    @if ($person->signData->count() > 0)
-                        @foreach ($person->signData as $personSign)
-                            <tr class="align-middle">
-                                <td>{{ $doc->docTypeName }} {{ $doc->docName }}</td>
-                                <td>{{ $person->personData->persondata_unit_name }}</td>
-                                <td>{{ $person->personData->persondata_fullname }}<br>{{ $person->personData->persondata_appointment }}</td>
-                                <td>{{ $personSign->signDateTime ?? '' }}</td>
-                                <td>
-                                    <div class="stamp">
-                                        <div class="d-flex justify-content-start stampmain">
-                                            <div class="p2"><img src="{{ asset('osfrportal/images/logo.svg') }}"
-                                                    alt=""></div>
-                                            <div class="p2">&nbsp;</div>
-                                            <div class="align-self-center p2">Документ подписан электронной подписью
-                                                ({{ $personSign->signLabel ?? '' }})
+    <div class="card w-100">
+        <h5 class="card-header text-center">
+            Ведомость ознакомления работников с нормативными документами<br>
+        </h5>
+        <div class="card-header">
+            Дата формирования: {{ \Carbon\Carbon::now()->format('d.m.Y') }}
+        </div>
+        <div class="card-body px-0 text-center">
+            <table class="table table-hover table-responsive">
+                <thead>
+                    <tr class="align-middle">
+                        <th scope="col">Документ</th>
+                        <th scope="col">Подразделение</th>
+                        <th scope="col">Работник</th>
+                        <th scope="col">Дата ознакомления</th>
+                        <th scope="col">Подпись</th>
+                    </tr>
+                </thead>
+                <tbody class="table-group-divider">
+                    @foreach ($allDocsArray as $doc)
+                        @foreach ($doc->docPersonSigns as $person)
+                            @if ($person->signData->count() > 0)
+                                @foreach ($person->signData as $personSign)
+                                    <tr class="align-middle">
+                                        <td>{{ $doc->docTypeName }} {{ $doc->docName }}</td>
+                                        <td>{{ $person->personData->persondata_unit_name }}</td>
+                                        <td>{{ $person->personData->persondata_fullname }}<br>{{ $person->personData->persondata_appointment }}
+                                        </td>
+                                        <td>{{ $personSign->signDateTime ?? '' }}</td>
+                                        <td>
+                                            <div class="stamp">
+                                                <div class="d-flex justify-content-start stampmain">
+                                                    <div class="p2"><img src="{{ asset('osfrportal/images/logo.svg') }}"
+                                                            alt=""></div>
+                                                    <div class="p2">&nbsp;</div>
+                                                    <div class="align-self-center p2">Документ подписан электронной подписью
+                                                        ({{ $personSign->signLabel ?? '' }})
+                                                    </div>
+                                                </div>
+                                                <table class="stampbottom">
+                                                    <tr>
+                                                        <td>Сертификат:</td>
+                                                        <td>{{ $personSign->signCertHash ?? '' }}</td>
+                                                    </tr>
+                                                    <tr class="stampbold">
+                                                        <td>Владелец:</td>
+                                                        <td>{{ $personSign->signCertCN ?? '' }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Действителен:</td>
+                                                        <td>{{ $personSign->signCertValidDates ?? '' }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Выдан:</td>
+                                                        <td>{{ $personSign->signIssuerCN ?? '' }}</td>
+                                                    </tr>
+                                                </table>
                                             </div>
-                                        </div>
-                                        <table class="stampbottom">
-                                            <tr>
-                                                <td>Сертификат:</td>
-                                                <td>{{ $personSign->signCertHash ?? '' }}</td>
-                                            </tr>
-                                            <tr class="stampbold">
-                                                <td>Владелец:</td>
-                                                <td>{{ $personSign->signCertCN ?? '' }}</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Действителен:</td>
-                                                <td>{{ $personSign->signCertValidDates ?? '' }}</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Выдан:</td>
-                                                <td>{{ $personSign->signIssuerCN ?? '' }}</td>
-                                            </tr>
-                                        </table>
-                                    </div>
-                                </td>
-                            </tr>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            @else
+                                <tr class="align-middle alert alert-danger">
+                                    <td>{{ $doc->docTypeName }} {{ $doc->docName }}</td>
+                                    <td>{{ $person->personData->persondata_unit_name }}</td>
+                                    <td>{{ $person->personData->persondata_fullname }}<br>{{ $person->personData->persondata_appointment }}
+                                    </td>
+                                    <td colspan="2">Не ознакомлен</td>
+                                </tr>
+                            @endif
                         @endforeach
-                    @else
-                        <tr class="align-middle alert alert-danger">
-                            <td>{{ $doc->docTypeName }} {{ $doc->docName }}</td>
-                            <td>{{ $person->personData->persondata_unit_name }}</td>
-                            <td>{{ $person->personData->persondata_fullname }}<br>{{ $person->personData->persondata_appointment }}</td>
-                            <td colspan="2">Не ознакомлен</td>
-                        </tr>
-                    @endif
-                @endforeach
-            @endforeach
-        </tbody>
-    </table>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
 @push('header-css')
     <style>
