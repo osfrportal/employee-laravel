55c760b785dca48cacab6f6c787af60f2c210a32
osfrportal <pleshkovpa@48.sfr.gov.ru>
2023-11-28 18:54:07 +0300

    some changes

--- resources/views/admin/docs/reports/parts/reportbyunitdoc.blade.php
+++ resources/views/admin/docs/reports/parts/reportbyunitdoc.blade.php
@@ -12,7 +12,26 @@
     </div>
 @endsection
 @section('content')
-    @foreach ($allDocsArray as $doc)
-        @dump($doc)
-    @endforeach
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th scope="col">Документ</th>
+                <th scope="col">Работник</th>
+                <th scope="col">Дата ознакомления</th>
+                <th scope="col">Подпись</th>
+            </tr>
+        </thead>
+        <tbody class="table-group-divider">
+            @foreach ($allDocsArray as $doc)
+                @foreach ($doc->docPersonSigns as $person)
+                    <tr>
+                        <td>{{ $doc->docTypeName }} {{ $doc->docName }}</td>
+                        <td>@dump($doc)</td>
+                        <td>@dump($person)</td>
+                        <td>@mdo</td>
+                    </tr>
+                @endforeach
+            @endforeach
+        </tbody>
+    </table>
 @endsection
