cbe044be32beef75bcf2daa29e5559e78a11a7d5
osfrportal <pleshkovpa@48.sfr.gov.ru>
2023-12-02 14:07:45 +0300

    some changes

--- resources/views/sections/dashboard/notifications/notifications_unread.blade.php
+++ resources/views/sections/dashboard/notifications/notifications_unread.blade.php
@@ -1,5 +1,5 @@
 <div class="card bg-body rounded shadow-sm" id="SFRnotificationsList">
-    <div class="card-header">
+    <div class="card-header bg-body">
         <h6 class="card-title">Уведомления</h6>
     </div>
     <div class="list-group list-group-flush list-group-hoverable">
@@ -14,19 +14,8 @@
                         <div class="d-block text-secondary text-truncate mt-n1">{{ $notificationDetail['data']['message'] ?? '' }}</div>
                     </div>
                     <div class="col-auto">
-                        <a href="#"
-                            class="list-group-item-actions"><!-- Download SVG icon from http://tabler-icons.io/i/star -->
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon text-secondary" width="24"
-                                height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                                stroke-linecap="round" stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                <path
-                                    d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z">
-                                </path>
-                            </svg>
-                        </a>
                         <button type="button" class="btn btn-link btn-sm mark-as-read"
-                        data-id="{{ $notificationDetail['id'] }}">Отметить прочитанным</button>
+                        data-id="{{ $notificationDetail['id'] }}"><i class="bi bi-check-lg"></i></button>
                     </div>
                 </div>
             </div>
