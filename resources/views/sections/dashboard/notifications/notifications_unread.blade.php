04b1566223f0e42b07349090dcaeb9128bdd78b8
osfrportal <pleshkovpa@48.sfr.gov.ru>
2023-12-02 14:30:40 +0300

    some changes

--- resources/views/sections/dashboard/notifications/notifications_unread.blade.php
+++ resources/views/sections/dashboard/notifications/notifications_unread.blade.php
@@ -8,7 +8,7 @@
                 <div class="list-group-item notifblock">
                     <div class="row align-items-center">
                         <div class="col text-truncate">
-                            <small class="text-muted d-block">{{ \Carbon\Carbon::parse($notificationDetail['updated_at'])->format('d.m.Y H:i:s')->timezone('Europe/Moscow') }}</small>
+                            <small class="text-muted d-block">{{ \Carbon\Carbon::parse($notificationDetail['updated_at'])->format('d.m.Y H:i:s')->tz('Europe/Moscow') }}</small>
                             <div class="d-block text-truncate">
                                 {{ $notificationDetail['data']['message'] ?? '' }}</div>
                         </div>
