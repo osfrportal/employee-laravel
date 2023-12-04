2247468e09057216daf9b3c76825ce717da9a0b6
osfrportal <pleshkovpa@48.sfr.gov.ru>
2023-12-04 17:18:26 +0300

    some changes

--- resources/views/livewire/docsnotsigned-count.blade.php
+++ resources/views/livewire/docsnotsigned-count.blade.php
@@ -1,6 +1,12 @@
 <div wire:poll.60s>
-    <i class="ti ti-file-certificate iconsMenu icon-tada icon-size-32"></i>
-    <span class="position-absolute start-100 translate-middle badge rounded-pill bg-danger">
-        {{ $docsNotSignedCount }}
-    </span>
+    <div class="my-3 p-3 bg-body rounded shadow-sm text-center" style="max-width: 12rem;" wire:poll.5s>
+        <div class="card-header">Активные пользователи</div>
+        <div class="card-body text-primary">
+            <i class="ti ti-file-certificate iconsMenu icon-tada icon-size-32"></i><span class="position-absolute start-100 translate-middle badge rounded-pill bg-danger">{{ $docsNotSignedCount }}</span>
+        </div>
+    </div>
+
+
+
+
 </div>
