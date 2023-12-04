e60061679cb310ced6f993137b0789a087effacd
osfrportal <pleshkovpa@48.sfr.gov.ru>
2023-12-04 19:04:59 +0300

    some changes

--- src/Livewire/DocsNotSigned.php
+++ src/Livewire/DocsNotSigned.php
@@ -36,7 +36,7 @@ class DocsNotSigned extends Component
         $this->docsNotSignedCount = $docsUnsignedCount;
 
         $text_to = sprintf('Вам необходимо ознакомиться с нормативными документами. Кол-во документов: %s', $this->docsNotSignedCount);
-        $this->flasher_interface->addWarning($text_to);
+        flash()->addWarning($text_to);
 /*
         sweetalert()
             ->timer(0)
