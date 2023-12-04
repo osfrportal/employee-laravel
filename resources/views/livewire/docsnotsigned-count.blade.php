c469b9d9e5febf208615cbdd7abd87dab2fef652
osfrportal <pleshkovpa@48.sfr.gov.ru>
2023-12-04 19:16:07 +0300

    some changes

--- resources/views/livewire/docsnotsigned-count.blade.php
+++ resources/views/livewire/docsnotsigned-count.blade.php
@@ -3,7 +3,7 @@
         <a class="nav-link position-relative" href="{{ route('osfrportal.docs.index') }}" alt="Документы для ознакомления">
             <span
                 class="position-absolute start-100 translate-middle badge rounded-pill bg-danger" title="Документов для ознакомления {{ $docsNotSignedCount }}">{{ $docsNotSignedCount }}</span>
-            <i class="ti ti-file-certificate iconsMenu icon-tada icon-size-32"></i>
+            <i class="ti ti-file-certificate iconsMenu icon-tada icon-size-32" title="Документов для ознакомления {{ $docsNotSignedCount }}"></i>
         </a>
     @endif
 </div>
