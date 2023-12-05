b0f591786629376865dcfa088b77c2b3b051060e
Paul A. Pleshkov <pleshkovpa@yandex.ru>
2023-12-05 23:09:23 +0300

    sc

--- src/Actions/Orion/GetAccessPointsByCardIdAction.php
+++ src/Actions/Orion/GetAccessPointsByCardIdAction.php
@@ -7,12 +7,18 @@ use Osfrportal\OsfrportalLaravel\Data\Orion\TKeyData;
 use Osfrportal\OsfrportalLaravel\Data\Orion\TAccessLevelData;
 use Osfrportal\OsfrportalLaravel\Data\Orion\TAccessLevelItemData;
 use Osfrportal\OsfrportalLaravel\Models\SfrOrionCards;
+use Illuminate\Database\Eloquent\ModelNotFoundException; 
+
 class GetAccessPointsByCardIdAction
 {
     use AsAction;
 
     public function handle(int $cardId) {
-        $cardData = SfrOrionCards::where('keyid',$cardId)->firstOrFail;
+        try {
+            $cardData = SfrOrionCards::where('keyid', $cardId)->firstOrFail;
+        } catch (ModelNotFoundException $e) {
+            dump($e);
+        }
         dump($cardData->OrionAccessLevel->taccessleveldata);
     }
 
