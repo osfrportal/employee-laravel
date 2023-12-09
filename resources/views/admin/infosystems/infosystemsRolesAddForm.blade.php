74ff7401f111f28bb51568c7a2cc76db2f5130e6
osfrportal <pleshkovpa@48.sfr.gov.ru>
2023-12-09 18:46:18 +0300

    some changes

--- resources/views/admin/infosystems/infosystemsRolesAddForm.blade.php
+++ resources/views/admin/infosystems/infosystemsRolesAddForm.blade.php
@@ -13,7 +13,27 @@
 @endsection
 @section('content')
     <form method="POST" action="{{ route('osfrportal.admin.infosystems.roles.save') }}">
-
+        <input type="hidden" id="iroleid" name="iroleid" value="{{ old('iroleid', Str::uuid()) }}">
+        <div class="mb-3 row">
+            <label for="irole_name" class="col-sm-2 col-form-label">Наименование роли</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control @error('irole_name') is-invalid @enderror" id="irole_name"
+                    name="irole_name" value="{{ old('irole_name') }}">
+                @error('irole_name')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+        </div>
+        <div class="mb-3 row">
+            <label for="isysid" class="col-sm-2 col-form-label">Информационная система</label>
+            <div class="col-sm-10">
+                <select class="form-select @error('isysid') is-invalid @enderror" id="isysid" name="isysid">
+                </select>
+                @error('isysid')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+        </div>
         <a class="btn btn-primary" href="{{ route('osfrportal.admin.infosystems.index') }}" role="button">Назад</a>
         <input class="btn btn-primary" type="submit" value="Сохранить">
     </form>
