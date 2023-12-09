00928be0a291af84335ced2935bba21e6e502515
Paul A. Pleshkov <pleshkovpa@yandex.ru>
2023-12-09 23:02:04 +0300

    sc

--- src/Actions/Api/Select2InfosystemByIDAction.php
+++ src/Actions/Api/Select2InfosystemByIDAction.php
@@ -6,6 +6,7 @@ use Osfrportal\OsfrportalLaravel\Models\SfrInfoSystems;
 use Osfrportal\OsfrportalLaravel\Http\Resources\SFRSelect2InfosystemByIDCollection;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 
 class Select2InfosystemByIDAction
 {
@@ -13,17 +14,22 @@ class Select2InfosystemByIDAction
 
     public function handle($isysid)
     {
-        $infosystemCollection = new Collection();
+        $isUuid = Str::isUuid($isysid);
+        if($isUuid) {
+            $infosystemCollection = new Collection();
 
-        $infosystem = SfrInfoSystems::where(['isysid', $isysid])->firstOrFail();
-        $tmp_arr = [];
-        $tmp_arr = Arr::add($tmp_arr, 'id', $infosystem->isysid);
-        $tmp_arr = Arr::add($tmp_arr, 'text', $infosystem->isys_name);
-        $infosystemCollection->push($tmp_arr);
+            $infosystem = SfrInfoSystems::where('isysid', $isysid)->firstOrFail();
+            $tmp_arr = [];
+            $tmp_arr = Arr::add($tmp_arr, 'id', $infosystem->isysid);
+            $tmp_arr = Arr::add($tmp_arr, 'text', $infosystem->isys_name);
+            $infosystemCollection->push($tmp_arr);
 
-        $api_data = $infosystemCollection->values()->all();
-
+            $api_data = $infosystemCollection->values()->all();
+        } else {
+            $api_data = [];
+        }
         return new SFRSelect2InfosystemByIDCollection($api_data);
+
     }
 
 }
