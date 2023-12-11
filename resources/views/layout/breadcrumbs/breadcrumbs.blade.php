95782ef5ee6484588f19de96b0dbe53a98ea089d
Paul A. Pleshkov <pleshkovpa@yandex.ru>
2023-12-11 22:04:22 +0300

    sc

--- resources/views/layout/breadcrumbs/breadcrumbs.blade.php
+++ resources/views/layout/breadcrumbs/breadcrumbs.blade.php
@@ -5,7 +5,7 @@
                 @foreach ($breadcrumbs as $breadcrumb)
                     @if (!$loop->last)
                         @if (!is_null($breadcrumb->url))
-                            <li class="breadcrumb-item"><a href="@{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a></li>
+                            <li class="breadcrumb-item"><a href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a></li>
                         @else
                             <li class="breadcrumb-item">{{ $breadcrumb->title }}</li>
                         @endif
