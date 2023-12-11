e199272aced83ffe507d7614eeb6295e328f6cef
Paul A. Pleshkov <pleshkovpa@yandex.ru>
2023-12-11 23:04:36 +0300

    sc

--- routes/osfrportal_breadcrumbs.php
+++ routes/osfrportal_breadcrumbs.php
@@ -88,9 +88,11 @@ Breadcrumbs::for('osfrportal.phone.index', function (BreadcrumbTrail $trail): vo
     $trail->push('Телефонный справочник', route('osfrportal.phone.index'));
 });
 
-Breadcrumbs::for('osfrportal.phone.editform', function (BreadcrumbTrail $trail, $personid): void {
+Breadcrumbs::for('osfrportal.phone.editform', function (BreadcrumbTrail $trail, $personname): void {
     $trail->parent('osfrportal.phone.index');
-    $trail->push('Редактирование контактной информации', route('osfrportal.phone.editform', $personid));
+    //$trail->push('Редактирование контактной информации', route('osfrportal.phone.editform', $personid));
+    $trail->push($personid);
+    $trail->push('Редактирование контактной информации');
 });
 
 
