bb240ef6b8432451d730ddce83728604d42c6129
Paul A. Pleshkov <pleshkovpa@yandex.ru>
2023-12-11 21:58:45 +0300

    sc

--- resources/views/layout/breadcrumbs/breadcrumbs.blade.php
+++ resources/views/layout/breadcrumbs/breadcrumbs.blade.php
@@ -1,15 +1,19 @@
 @unless ($breadcrumbs->isEmpty())
-<div class="pt-0">
-    <nav aria-label="breadcrumb">
-        <ol class="breadcrumb">
-            @foreach ($breadcrumbs as $breadcrumb)
-                @if (!is_null($breadcrumb->url) && !$loop->last)
-                    <li class="breadcrumb-item"><a href="@{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a></li>
-                @else
-                    <li class="breadcrumb-item active">{{ $breadcrumb->title }}</li>
-                @endif
-            @endforeach
-        </ol>
-    </nav>
-</div>
+    <div class="pt-0">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                @foreach ($breadcrumbs as $breadcrumb)
+                    @if (!$loop->last)
+                        @if (!is_null($breadcrumb->url))
+                            <li class="breadcrumb-item"><a href="@{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a></li>
+                        @else
+                            <li class="breadcrumb-item">{{ $breadcrumb->title }}</li>
+                        @endif
+                    @else
+                        <li class="breadcrumb-item active">{{ $breadcrumb->title }}</li>
+                    @endif
+                @endforeach
+            </ol>
+        </nav>
+    </div>
 @endunless
