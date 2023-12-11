606be645d5082e55d6842188bcff129c26c62caf
Paul A. Pleshkov <pleshkovpa@yandex.ru>
2023-12-11 21:37:30 +0300

    sc

--- routes/osfrportal_breadcrumbs.php
+++ routes/osfrportal_breadcrumbs.php
@@ -2,6 +2,11 @@
 use Diglactic\Breadcrumbs\Breadcrumbs;
 use Diglactic\Breadcrumbs\Generator as BreadcrumbTrail;
 
+Breadcrumbs::for('osfrportal.admin', function (BreadcrumbTrail $trail): void {
+    $trail->push('Администрирование');
+});
+
 Breadcrumbs::for('osfrportal.admin.infosystems.index', function (BreadcrumbTrail $trail): void {
+    $trail->parent('osfrportal.admin');
     $trail->push('Информационные системы', route('osfrportal.admin.infosystems.index'));
 });
