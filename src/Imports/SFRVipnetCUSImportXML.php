2161eef336aff21a254cbc66c2f23a66d4784c3f
osfrportal <pleshkovpa@48.sfr.gov.ru>
2023-12-15 19:10:17 +0300

    some changes

--- src/Imports/SFRVipnetCUSImportXML.php
+++ src/Imports/SFRVipnetCUSImportXML.php
@@ -47,6 +47,8 @@ class SFRVipnetCUSImportXML
                 $clientID = $client->attributes()->id;
                 $clientName = Str::squish(Str::remove('058 - ', $client->attributes()->name));
                 $collection = Str::of($clientName)->explode(' ');
+                $re = '/^(\S+)\s+(\S+)\s+(\S+)$/im';
+                $collection = Str::of($clientName)->match($re);
                 $str_to_dump = sprintf('%s id: %s name: %s', $businessMailMessage, $clientID, $clientName);
                 dump($str_to_dump);
                 dump($collection);
