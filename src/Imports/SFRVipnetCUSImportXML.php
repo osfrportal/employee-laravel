91f3491267b69eebff440bb8bf97c16e45354016
osfrportal <pleshkovpa@48.sfr.gov.ru>
2023-12-15 19:52:13 +0300

    some changes

--- src/Imports/SFRVipnetCUSImportXML.php
+++ src/Imports/SFRVipnetCUSImportXML.php
@@ -53,7 +53,7 @@ class SFRVipnetCUSImportXML
                 $filtered = Arr::except($nameArray, [0]);
                 if (count($filtered) == 3 && $hasBusinessMail) {
                     $model = SfrPerson::where(['psurname'=> $filtered[1],'pname' => $filtered[2], 'pmiddlename' => $filtered[3]])->get('pid');
-                    if (Str::isUuid($model->pid)) {
+                    if (Str::isUuid($model->get('pid'))) {
                         $str_to_dump = sprintf('PID: %s, %s id: %s name: %s', $model->pid, $businessMailMessage, $clientID, $clientName);
                         dump($str_to_dump);
                     } else {
