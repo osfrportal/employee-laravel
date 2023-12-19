f2202e3fd6a3a925d35dcaef080ada305501e9c1
Paul A. Pleshkov <pleshkovpa@yandex.ru>
2023-12-19 23:55:14 +0300

    sc

--- src/Livewire/Admin/RedisSubscribeMainterance.php
+++ src/Livewire/Admin/RedisSubscribeMainterance.php
@@ -4,20 +4,28 @@ namespace Osfrportal\OsfrportalLaravel\Livewire\Admin;
 use Livewire\Component;
 
 use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Auth;
 
 
 class RedisSubscribeMainterance extends Component
 {
     public $redis_message;
+    public $durationInSeconds;
+
     public function __construct()
     {
             $this->redis_message = '';
+        $this->durationInSeconds = 35;
     }
     public function render()
     {
+        $userId = Auth::user()->id;
+        $key = "admin:mainterance:{$userId}";
+
         $msg = json_encode(array('time' => 'time_message', 'message' => 'text_message'));
-        Redis::set('admin:mainterance:123', $msg, 'EX', 35);
-        $this->redis_message = Redis::get('admin:mainterance:123');
+        Redis::setex($key, $this->durationInSeconds, $userId);
+        //Redis::set($key, $msg, 'EX', 35);
+        $this->redis_message = Redis::get($key);
         return view('osfrportal::livewire.admin.mainterance-messages', ['redis_message' => $this->redis_message]);
     }
 }
