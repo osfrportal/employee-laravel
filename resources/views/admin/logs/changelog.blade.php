0b8f78eea13978c793378527e632ff48c0031d48
osfrportal <pleshkovpa@48.sfr.gov.ru>
2023-12-21 09:17:46 +0300

    some changes

--- resources/views/admin/logs/changelog.blade.php
+++ resources/views/admin/logs/changelog.blade.php
@@ -10,10 +10,29 @@
                     dy=".3em">32x32</text>
             </svg>
             <div class="pb-3 mb-0  lh-sm border-bottom w-100">
+                <span class="d-block  mb-2">21-12-2023</span>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">
+                        <i class="ti ti-code-asterix icon-size-16" title="[FIX]"></i> Исправлена передача номеров
+                        сертификатов УКЭП для подписания документов. Отозванные номера не передаются.
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="d-flex text-muted pt-3 notifblock">
+            <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32"
+                xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32"
+                preserveAspectRatio="xMidYMid slice" focusable="false">
+                <title>Placeholder</title>
+                <rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff"
+                    dy=".3em">32x32</text>
+            </svg>
+            <div class="pb-3 mb-0  lh-sm border-bottom w-100">
                 <span class="d-block  mb-2">17-12-2023</span>
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">
-                        <i class="ti ti-code-dots icon-size-16" title="[TEST]"></i> В раздел бизнес ресурсы загружена информация по АП VipNet
+                        <i class="ti ti-code-dots icon-size-16" title="[TEST]"></i> В раздел бизнес ресурсы загружена
+                        информация по АП VipNet
                     </li>
                 </ul>
             </div>
@@ -30,7 +49,8 @@
                 <span class="d-block  mb-2">11-12-2023</span>
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">
-                        <i class="ti ti-code-plus icon-size-16" title="[ADD]"></i> Добавлен импорт файла с датами начала работы + синхронизация этой даты в ADOC
+                        <i class="ti ti-code-plus icon-size-16" title="[ADD]"></i> Добавлен импорт файла с датами начала
+                        работы + синхронизация этой даты в ADOC
                     </li>
                 </ul>
             </div>
@@ -51,7 +71,8 @@
                         работникам о необходимости ознакомления с документами
                     </li>
                     <li class="list-group-item">
-                        <i class="ti ti-code-plus icon-size-16" title="[ADD]"></i> Добавлено отображение списка точек доступа при просмотре подробной информации по карте СКУД в административном интерфейсе
+                        <i class="ti ti-code-plus icon-size-16" title="[ADD]"></i> Добавлено отображение списка точек
+                        доступа при просмотре подробной информации по карте СКУД в административном интерфейсе
                     </li>
                 </ul>
             </div>
@@ -86,7 +107,8 @@
                 <span class="d-block  mb-2">21-11-2023</span>
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">
-                        <i class="ti ti-code-asterix icon-size-16" title="[FIX]"></i> Исправлено отображение шрифта Montserrat в браузере Firefox (проверено на win10 firefox 120)
+                        <i class="ti ti-code-asterix icon-size-16" title="[FIX]"></i> Исправлено отображение шрифта
+                        Montserrat в браузере Firefox (проверено на win10 firefox 120)
                     </li>
                     <li class="list-group-item">
                         <i class="ti ti-code-plus icon-size-16" title="[ADD]"></i> При просмотре списка работников и
@@ -107,10 +129,12 @@
                 <span class="d-block  mb-2">20-11-2023</span>
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">
-                        <i class="ti ti-code-asterix icon-size-16" title="[FIX]"></i> Портал переведен на использование корпоративного шрифта Montserrat
+                        <i class="ti ti-code-asterix icon-size-16" title="[FIX]"></i> Портал переведен на использование
+                        корпоративного шрифта Montserrat
                     </li>
                     <li class="list-group-item">
-                        <i class="ti ti-code-asterix icon-size-16" title="[FIX]"></i> Скрипты и стили перенесены на внутренний ресурс для ускорения загрузки
+                        <i class="ti ti-code-asterix icon-size-16" title="[FIX]"></i> Скрипты и стили перенесены на
+                        внутренний ресурс для ускорения загрузки
                     </li>
                 </ul>
             </div>
@@ -171,11 +195,13 @@
                         отображение количества документов, с которыми работник еще не ознакомился.
                     </li>
                     <li class="list-group-item">
-                        <i class="ti ti-code-asterix icon-size-16" title="[FIX]"></i> Исправлено отображение ведомости ознакомления и профиля работника в
+                        <i class="ti ti-code-asterix icon-size-16" title="[FIX]"></i> Исправлено отображение ведомости
+                        ознакомления и профиля работника в
                         интерфейсе администратора. (ошибка в случае ознакомления рабоника УНЭПом)
                     </li>
                     <li class="list-group-item">
-                        <i class="ti ti-code-dots icon-size-16" title="[TEST]"></i> Добавлены уведомления о синхронизации. (уведомления пользователям
+                        <i class="ti ti-code-dots icon-size-16" title="[TEST]"></i> Добавлены уведомления о синхронизации.
+                        (уведомления пользователям
                         группы system-notifications)
                     </li>
                     <li class="list-group-item">
