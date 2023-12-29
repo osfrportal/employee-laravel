4065b5dbe53e178c00646288ce527b5f1c15b94b
Paul Pleshkov <pleshkovpa@yandex.ru>
2023-12-29 18:51:37 +0300

    sc

--- resources/views/admin/crypto/cryptoall.blade.php
+++ resources/views/admin/crypto/cryptoall.blade.php
@@ -75,7 +75,7 @@
                     {
                         targets: 8,
                         render: function(data, type, row, meta) {
-                            if (!row.pid) {
+                            if (row.pid !== 'null') {
                                 var personProfileUrl =
                                     '{{ route('osfrportal.admin.persons.detail', ':slug') }}';
                                 personProfileUrl = personProfileUrl.replace(':slug', row.pid);
