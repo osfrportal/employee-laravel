9109b4a2a2830a5f81522b2d6eab401bfede2e98
Paul Pleshkov <pleshkovpa@yandex.ru>
2023-12-30 00:25:30 +0300

    sc

--- resources/views/admin/crypto/cryptoall.blade.php
+++ resources/views/admin/crypto/cryptoall.blade.php
@@ -27,7 +27,7 @@
                 ordering: true,
                 columnDefs: [{
                         className: "dt-center",
-                        targets: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+                        targets: [0, 1, 2, 3, 4, 5, 6],
                         orderable: true,
                         searchable: true,
                     },
