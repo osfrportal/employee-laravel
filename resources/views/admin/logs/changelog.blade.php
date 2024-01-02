d98e22a775474567524556fb19ec1b1784d041a9
Paul Pleshkov <pleshkovpa@yandex.ru>
2024-01-03 02:59:52 +0300

    sc

--- resources/views/admin/logs/changelog.blade.php
+++ resources/views/admin/logs/changelog.blade.php
@@ -48,81 +48,6 @@
                     dy=".3em">32x32</text>
             </svg>
             <div class="pb-3 mb-0  lh-sm border-bottom w-100">
-                <span class="d-block  mb-2">02-01-2024</span>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">
-                        <i class="ti ti-code-plus icon-size-24" title="[ADD]"></i> Добавлен раздел Бизнес-ресурсы
-                        &RightArrow; Криптосредства.
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="d-flex text-muted pt-3 notifblock">
-            <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32"
-                xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32"
-                preserveAspectRatio="xMidYMid slice" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff"
-                    dy=".3em">32x32</text>
-            </svg>
-            <div class="pb-3 mb-0  lh-sm border-bottom w-100">
-                <span class="d-block  mb-2">21-12-2023</span>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">
-                        <i class="ti ti-code-plus icon-size-24" title="[ADD]"></i> Добавлено поле "Дата окончания действия
-                        документа" в раздел нормативных документов.
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="d-flex text-muted pt-3 notifblock">
-            <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32"
-                xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32"
-                preserveAspectRatio="xMidYMid slice" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff"
-                    dy=".3em">32x32</text>
-            </svg>
-            <div class="pb-3 mb-0  lh-sm border-bottom w-100">
-                <span class="d-block  mb-2">21-12-2023</span>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">
-                        <i class="ti ti-code-asterix icon-size-24" title="[FIX]"></i> Исправлена передача номеров
-                        сертификатов УКЭП для подписания документов. Отозванные номера не передаются.
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="d-flex text-muted pt-3 notifblock">
-            <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32"
-                xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32"
-                preserveAspectRatio="xMidYMid slice" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff"
-                    dy=".3em">32x32</text>
-            </svg>
-            <div class="pb-3 mb-0  lh-sm border-bottom w-100">
-                <span class="d-block  mb-2">17-12-2023</span>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">
-                        <i class="ti ti-code-dots icon-size-24" title="[TEST]"></i> В раздел Бизнес-ресурсы
-                        &RightArrow; Криптосредства загружена
-                        информация по АП VipNet
-                    </li>
-                </ul>
-            </div>
-        </div>
-
-
-        <div class="d-flex text-muted pt-3 notifblock">
-            <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32"
-                xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32"
-                preserveAspectRatio="xMidYMid slice" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff"
-                    dy=".3em">32x32</text>
-            </svg>
-            <div class="pb-3 mb-0  lh-sm border-bottom w-100">
                 <span class="d-block  mb-2">21-11-2023</span>
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">
@@ -167,24 +92,6 @@
                     dy=".3em">32x32</text>
             </svg>
             <div class="pb-3 mb-0  lh-sm border-bottom w-100">
-                <span class="d-block  mb-2">15-11-2023</span>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">
-                        <i class="ti ti-code-plus icon-size-24" title="[ADD]"></i> Логи / Журнал обновления телефонного
-                        справочника
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="d-flex text-muted pt-3 notifblock">
-            <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32"
-                xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32"
-                preserveAspectRatio="xMidYMid slice" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff"
-                    dy=".3em">32x32</text>
-            </svg>
-            <div class="pb-3 mb-0  lh-sm border-bottom w-100">
                 <span class="d-block  mb-2">11-11-2023</span>
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">
@@ -194,38 +101,6 @@
 
                     <li class="list-group-item">
                         <i class="ti ti-code-plus icon-size-24" title="[ADD]"></i> Добавлено журналирование в БД
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="d-flex text-muted pt-3 notifblock">
-            <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32"
-                xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32"
-                preserveAspectRatio="xMidYMid slice" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff"
-                    dy=".3em">32x32</text>
-            </svg>
-            <div class="pb-3 mb-0  lh-sm border-bottom w-100">
-                <span class="d-block  mb-2">08-11-2023</span>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">
-                        <i class="ti ti-code-plus icon-size-24" title="[ADD]"></i> В личном кабинете добавлено
-                        отображение количества документов, с которыми работник еще не ознакомился.
-                    </li>
-                    <li class="list-group-item">
-                        <i class="ti ti-code-asterix icon-size-24" title="[FIX]"></i> Исправлено отображение ведомости
-                        ознакомления и профиля работника в
-                        интерфейсе администратора. (ошибка в случае ознакомления рабоника УНЭПом)
-                    </li>
-                    <li class="list-group-item">
-                        <i class="ti ti-code-dots icon-size-24" title="[TEST]"></i> Добавлены уведомления о синхронизации.
-                        (уведомления пользователям
-                        группы system-notifications)
-                    </li>
-                    <li class="list-group-item">
-                        <i class="ti ti-code-plus icon-size-24" title="[ADD]"></i> Добавлено отображение списка
-                        сертификатов в базе.
                     </li>
                 </ul>
             </div>
