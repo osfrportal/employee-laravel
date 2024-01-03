5148ba93174f97bc9addc30a2024e02a3dba013e
Paul Pleshkov <pleshkovpa@yandex.ru>
2024-01-04 01:24:17 +0300

    sc

--- resources/views/admin/persons/movements/movements_all.blade.php
+++ resources/views/admin/persons/movements/movements_all.blade.php
@@ -41,9 +41,8 @@
                     className: 'dt-body-center',
                     render: function(data, type, full, meta) {
                         console.table(JSON.stringify(data));
-                        var jsondata = JSON.parse(data[0]);
-                        console.log(jsondata);
-                        return jsondata;
+                        console.table(data);
+                        return data;
                     }
                 }, ],
             });
