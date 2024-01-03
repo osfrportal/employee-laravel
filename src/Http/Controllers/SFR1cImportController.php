c89e837c21a925478fc174d5db2f2cbcf9da31cf
Paul Pleshkov <pleshkovpa@yandex.ru>
2024-01-04 00:10:14 +0300

    sc

--- src/Http/Controllers/SFR1cImportController.php
+++ src/Http/Controllers/SFR1cImportController.php
@@ -46,7 +46,7 @@ class SFR1cImportController extends Controller
     {
         parent::__construct();
         $this->now_date_for_import = Carbon::now(config('app.timezone', 'Europe/Moscow'))->format('Y-m-d');
-        $this->now_date_for_import = '2023-09-30';
+        $this->now_date_for_import = '2023-10-31';
         $this->absence_file_name = sprintf('otsutstvie_058 %s.txt', $this->now_date_for_import);
         $this->vacation_file_name = sprintf('vacation_058 %s.txt', $this->now_date_for_import);
         $this->dekret_file_name = sprintf('dekret_058 %s.txt', $this->now_date_for_import);
