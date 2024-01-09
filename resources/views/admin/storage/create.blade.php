2f7689bf814d7caa378b3eb7499876b9b5e5d21a
osfrportal <pleshkovpa@48.sfr.gov.ru>
2024-01-09 20:47:31 +0300

    some changes

--- resources/views/admin/storage/create.blade.php
+++ resources/views/admin/storage/create.blade.php
@@ -39,7 +39,7 @@
                     <div class="input-group">
                         <input class="form-control form-control-sm @error('storvolume') is-invalid @enderror"
                             id="storvolume" name="storvolume" type="text" value="{{ old('storvolume') ?? '' }}"
-                            size="12">
+                            length="12">
                         <span class="input-group-text" id="basic-addon2">Мегабайт</span>
                     </div>
                     @error('storvolume')
