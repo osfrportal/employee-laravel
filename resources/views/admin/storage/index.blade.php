b89b583af1599ed55f3ff61ca5c88f125827aef4
osfrportal <pleshkovpa@48.sfr.gov.ru>
2024-01-09 17:31:29 +0300

    some changes

--- resources/views/admin/storage/index.blade.php
+++ resources/views/admin/storage/index.blade.php
@@ -1,6 +1,19 @@
 @extends('osfrportal::layout')
 
 @section('content')
+    <div id="buttons_wrapper" class="mb-3">
+        <div class="row">
+            <div class="col-6 text-start">
+                <a class="btn btn-sm btn-primary" href="#"><i class="ti ti-device-usb icon-size-24"></i>
+                    Добавить</a>
+            </div>
+            <div class="col-6 text-end">
+                <a class="btn btn-sm btn-outline-primary" href="#"><i
+                        class="ti ti-device-desktop-analytics icon-size-24"></i>
+                    Сформировать журнал</a>
+            </div>
+        </div>
+    </div>
     <div class="pt-0">
         <table class="table table-sm dataTable no-footer" id="table-storage">
             <thead>
@@ -12,10 +25,10 @@
                     <th>Заводской или входящий номер</th>
                     <th>Емкость носителя</th>
                     <th>Откуда поступил</th>
-                    <th>Дата документа о снятии с учета</th>
-                    <th>Номер документа о снятии с учета</th>
                     <th>Работник</th>
                     <th>Отметка о проверке</th>
+                    <th>Дата документа о снятии с учета</th>
+                    <th>Номер документа о снятии с учета</th>
                 </tr>
             </thead>
         </table>
