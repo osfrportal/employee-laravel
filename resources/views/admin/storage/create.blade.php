dd29c9bde0f60a51e9bac5d64d4a674bda02e7de
osfrportal <pleshkovpa@48.sfr.gov.ru>
2024-01-09 19:01:42 +0300

    some changes

--- resources/views/admin/storage/create.blade.php
+++ resources/views/admin/storage/create.blade.php
@@ -36,8 +36,34 @@
                 </div>
             </div>
         </div>
+        <div class="card mb-4">
+            <div class="card-header">Работник</div>
+            <div class="card-body">
+                <select class="form-select form-select-sm mb-3" id="js-persons-ajax" name="personid"
+                    data-placeholder="Выберите работника" data-allow-clear="true" data-minimum-input-length="4"
+                    data-ajax--delay="500" data-language="ru" data-selection-css-class="select2--small"
+                    data-dropdown-css-class="select2--small"></select>
+            </div>
+        </div>
         <div class="mb-3">
             <input class="btn btn-primary" type="submit" value="Добавить">
         </div>
     </form>
 @endsection
+@push('footer-scripts')
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#js-persons-ajax').select2({
+                ajax: {
+                    dataType: 'json',
+                    url: function(params) {
+                        var urlroute =
+                            '{{ route('osfrapi.osfrportal.admin.select2.persons.search', ':slug') }}';
+                        urlroute = urlroute.replace(':slug', params.term);
+                        return urlroute;
+                    }
+                }
+            });
+        });
+    </script>
+@endpush
