24de4b4e36ec02fcce9b5a83e16b06e09254d56a
osfrportal <pleshkovpa@48.sfr.gov.ru>
2024-01-09 18:45:14 +0300

    some changes

--- resources/views/admin/storage/create.blade.php
+++ resources/views/admin/storage/create.blade.php
@@ -1,45 +1,43 @@
 @extends('osfrportal::layout')
 
 @section('content')
-    <div class="container">
-        <form method="POST" action="{{ route('osfrportal.admin.storage.store') }}">
-            @csrf
-            <div class="card mb-4">
-                <div class="card-header">Добавление устройства хранения</div>
-                <div class="card-body">
-                    <div class="mb-3">
-                        <label class="mb-1" for="stortype">Тип:</label>
-                        <select name="stortype" id="stortype"
-                            class="form-control form-control-sm @error('stortype') is-invalid @enderror">
-                            @foreach ($StorageTypes as $storageTypeKey => $storageType)
-                                <option value="{{ $storageTypeKey }}" @selected(old('stortype') == $storageTypeKey)>
-                                    {{ $storageType }}
-                                </option>
-                            @endforeach
-                        </select>
-                        @error('stortype')
-                            <div class="text-danger">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <div class="mb-3">
-                        <label class="mb-1" for="stormark">Метка категории носителя:</label>
-                        <select name="stormark" id="stormark"
-                            class="form-control form-control-sm @error('stormark') is-invalid @enderror">
-                            @foreach ($StorageCategoryTypes as $storageCategoryTypeKey => $storageCategoryType)
-                                <option value="{{ $storageCategoryTypeKey }}" @selected(old('stormark') == $storageCategoryTypeKey)>
-                                    {{ $storageCategoryType }}
-                                </option>
-                            @endforeach
-                        </select>
-                        @error('stormark')
-                            <div class="text-danger">{{ $message }}</div>
-                        @enderror
-                    </div>
+    <form method="POST" action="{{ route('osfrportal.admin.storage.store') }}">
+        @csrf
+        <div class="card mb-4">
+            <div class="card-header">Добавление устройства хранения</div>
+            <div class="card-body">
+                <div class="mb-3">
+                    <label class="mb-1" for="stortype">Тип:</label>
+                    <select name="stortype" id="stortype"
+                        class="form-control form-control-sm @error('stortype') is-invalid @enderror">
+                        @foreach ($StorageTypes as $storageTypeKey => $storageType)
+                            <option value="{{ $storageTypeKey }}" @selected(old('stortype') == $storageTypeKey)>
+                                {{ $storageType }}
+                            </option>
+                        @endforeach
+                    </select>
+                    @error('stortype')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label class="mb-1" for="stormark">Метка категории носителя:</label>
+                    <select name="stormark" id="stormark"
+                        class="form-control form-control-sm @error('stormark') is-invalid @enderror">
+                        @foreach ($StorageCategoryTypes as $storageCategoryTypeKey => $storageCategoryType)
+                            <option value="{{ $storageCategoryTypeKey }}" @selected(old('stormark') == $storageCategoryTypeKey)>
+                                {{ $storageCategoryType }}
+                            </option>
+                        @endforeach
+                    </select>
+                    @error('stormark')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
                 </div>
             </div>
-            <div class="mb-3">
-                <input class="btn btn-primary" type="submit" value="Добавить">
-            </div>
-        </form>
-    </div>
+        </div>
+        <div class="mb-3">
+            <input class="btn btn-primary" type="submit" value="Добавить">
+        </div>
+    </form>
 @endsection
