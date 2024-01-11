5a1f62cbaa6629c26ed10a2f12e16dcab5e09633
osfrportal <pleshkovpa@48.sfr.gov.ru>
2024-01-11 15:18:38 +0300

    some changes

--- src/Actions/Docs/CountUnsignedDocsByUserAction.php
+++ src/Actions/Docs/CountUnsignedDocsByUserAction.php
@@ -5,6 +5,11 @@ use Lorisleiva\Actions\Concerns\AsAction;
 
 use Osfrportal\OsfrportalLaravel\Models\SfrDocs;
 
+use Carbon\Carbon;
+
+use Osfrportal\OsfrportalLaravel\Data\SFRDocData;
+use Illuminate\Support\Facades\Auth;
+
 class CountUnsignedDocsByUserAction
 {
     use AsAction;
@@ -14,13 +19,18 @@ class CountUnsignedDocsByUserAction
         $allDocs = SfrDocs::where('doc_data->docNeedSign', true)->with(['SfrDocsFiles', 'SfrDocsUserSigns'])->get();
         $collectionDocs = collect();
         $collectionSigns = collect();
-        
+        $personWorkStartDate = Auth::user()->SfrPerson->getWorkStartDateCarbon();
+
         foreach ($allDocs as $doc) {
-            foreach ($doc->SfrDocsFiles as $f) {
-                $collectionDocs->push($f['fileid']);
-            }
-            foreach ($doc->SfrDocsUserSigns as $s) {
-                $collectionSigns->push($s['sign_fileid']);
+            $docDataDTO = SFRDocData::forList($doc);
+            $docDateEndCarbon = Carbon::parse($docDataDTO->docDateEnd);
+            if ($docDateEndCarbon->gte($personWorkStartDate)) {
+                foreach ($doc->SfrDocsFiles as $f) {
+                    $collectionDocs->push($f['fileid']);
+                }
+                foreach ($doc->SfrDocsUserSigns as $s) {
+                    $collectionSigns->push($s['sign_fileid']);
+                }
             }
         }
         $diffCollection = $collectionDocs->diff($collectionSigns);
