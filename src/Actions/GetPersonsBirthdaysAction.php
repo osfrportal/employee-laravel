d59866106a72b2c9ee27f5a430863d5573dc3e3f
Paul Pleshkov <pleshkovpa@yandex.ru>
2024-01-23 01:35:46 +0300

    ыс

--- src/Actions/GetPersonsBirthdaysAction.php
+++ src/Actions/GetPersonsBirthdaysAction.php
@@ -18,7 +18,7 @@ class GetPersonsBirthdaysAction
     public function handle()
     {
         $dateFrom = Carbon::now();
-        $dateTo = Carbon::now()->subDays(80);
+        $dateTo = Carbon::now()->subDays(150);
 
         $personsFromDB = SfrPerson::whereBetween('pbirthdate', [$dateFrom, $dateTo])->get();
         dump($personsFromDB);
