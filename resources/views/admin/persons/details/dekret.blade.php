0508e4b1ea402ac298f5407840e5b922ac65b780
Paul Pleshkov <pleshkovpa@yandex.ru>
2024-01-25 23:43:18 +0300

    sc

--- resources/views/admin/persons/details/dekret.blade.php
+++ resources/views/admin/persons/details/dekret.blade.php
@@ -3,7 +3,8 @@
         <div class="card-header">Декрет</div>
         <div class="card-body">
             @foreach ($SFRPersonDekret as $SFRPersonDekretRow)
-                {{ $SFRPersonDekretRow->dekretstart->format('d.m.Y') }} {{ $SFRPersonDekretRow->dekretend }}<br>
+                {{ \Carbon\Carbon::parse($SFRPersonDekretRow->dekretstart)->format('d.m.Y') }}
+                {{ $SFRPersonDekretRow->dekretend }}<br>
             @endforeach
         </div>
     </div>
