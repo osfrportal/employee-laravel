dc3683e8a2722a1009defd67289d85a64d4a3c00
Paul Pleshkov <pleshkovpa@yandex.ru>
2024-01-26 00:41:18 +0300

    sc

--- src/Providers/OsfrportalServiceProvider.php
+++ src/Providers/OsfrportalServiceProvider.php
@@ -148,6 +148,7 @@ class OsfrportalServiceProvider extends ServiceProvider
         ]);
         $this->registerLogToDBoptions();
         $this->registerBreadCrumbsConfig();
+        $this->registerADConfig();
     }
 
     protected function registerBladeDirectives()
