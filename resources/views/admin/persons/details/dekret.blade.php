1aec3e34daefc957265f6c7668238ab009a077fd
Paul Pleshkov <pleshkovpa@yandex.ru>
2024-01-25 23:39:16 +0300

    sc

--- resources/views/admin/persons/details/dekret.blade.php
+++ resources/views/admin/persons/details/dekret.blade.php
@@ -1,8 +1,10 @@
-<div class="card mb-4 mb-xl-0 mt-2">
-    <div class="card-header">Декрет</div>
-    <div class="card-body">
-        @foreach ($SFRPersonDekret as $SFRPersonDekretRow)
-            {{ $SFRPersonDekretRow->dekretstart }} {{ $SFRPersonDekretRow->dekretend }}<br>
-        @endforeach
+@if ($SFRPersonDekret->count() > 0)
+    <div class="card mb-4 mb-xl-0 mt-2">
+        <div class="card-header">Декрет</div>
+        <div class="card-body">
+            @foreach ($SFRPersonDekret as $SFRPersonDekretRow)
+                {{ $SFRPersonDekretRow->dekretstart }} {{ $SFRPersonDekretRow->dekretend }}<br>
+            @endforeach
+        </div>
     </div>
-</div>
+@endif
