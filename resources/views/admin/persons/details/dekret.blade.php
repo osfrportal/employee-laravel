eba6fae6d6a2e5daa1166a57bdf7819721e5dd13
osfrportal <pleshkovpa@48.sfr.gov.ru>
2024-01-25 20:15:37 +0300

    some changes

--- resources/views/admin/persons/details/dekret.blade.php
+++ resources/views/admin/persons/details/dekret.blade.php
@@ -1,6 +1,8 @@
 <div class="card mb-4 mb-xl-0 mt-2">
     <div class="card-header">Декрет</div>
     <div class="card-body">
-        @dump($SFRPersonDekret)
+        @foreach ($SFRPersonDekret as $SFRPersonDekretRow)
+            {{ $SFRPersonDekretRow->dekretstart }} {{ $SFRPersonDekretRow->dekretend }}<br>
+        @endforeach
     </div>
 </div>
