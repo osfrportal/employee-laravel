28e762c55408277ee22ebd8b7cbbce4119626fac
Paul Pleshkov <pleshkovpa@yandex.ru>
2024-01-25 23:46:43 +0300

    sc

--- resources/views/admin/persons/details/dekret.blade.php
+++ resources/views/admin/persons/details/dekret.blade.php
@@ -2,10 +2,18 @@
     <div class="card mb-4 mb-xl-0 mt-2">
         <div class="card-header">Декрет</div>
         <div class="card-body">
-            @foreach ($SFRPersonDekret as $SFRPersonDekretRow)
-                {{ \Carbon\Carbon::parse($SFRPersonDekretRow->dekretstart)->format('d.m.Y') }}
-                {{ $SFRPersonDekretRow->dekretend }}<br>
-            @endforeach
+            <div class="container text-center">
+                @foreach ($SFRPersonDekret as $SFRPersonDekretRow)
+                    <div class="row">
+                        <div class="col">
+                            c {{ \Carbon\Carbon::parse($SFRPersonDekretRow->dekretstart)->format('d.m.Y') }}
+                        </div>
+                        <div class="col">
+                            по {{ \Carbon\Carbon::parse($SFRPersonDekretRow->dekretend)->format('d.m.Y') }}
+                        </div>
+                    </div>
+                @endforeach
+            </div>
         </div>
     </div>
 @endif
