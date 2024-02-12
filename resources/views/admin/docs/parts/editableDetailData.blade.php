0b775348e786a0ab170317f10c9ae74c18581c06
osfrportal <pleshkovpa@48.sfr.gov.ru>
2024-02-12 19:53:16 +0300

    some changes

--- resources/views/admin/docs/parts/editableDetailData.blade.php
+++ resources/views/admin/docs/parts/editableDetailData.blade.php
@@ -7,3 +7,12 @@ editable
         </div>
     </div>
 </form>
+
+<form method="POST" action="{{ route('osfrportal.admin.docs.saveeditable') }}" enctype="multipart/form-data">
+    <input type="hidden" name="docid" id="docid" value="{{ $docid ?? '' }}">
+    <div class="mb-3 row">
+        <div class="col">
+            <button type="submit" class="btn btn-sm btn-danger">УДАЛИТЬ ДОКУМЕНТ И ФАЙЛЫ</button>
+        </div>
+    </div>
+</form>
