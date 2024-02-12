e04fc88a46f11a13a1bb2df1a68f62e3e4f73768
osfrportal <pleshkovpa@48.sfr.gov.ru>
2024-02-12 19:49:17 +0300

    some changes

--- resources/views/admin/docs/docs_detail.blade.php
+++ resources/views/admin/docs/docs_detail.blade.php
@@ -24,27 +24,6 @@
                 @else
                     @include('osfrportal::admin.docs.parts.detailData')
                 @endif
-                <form method="POST" action="{{ route('osfrportal.admin.docs.savedateend') }}"
-                    enctype="multipart/form-data">
-                    <input type="hidden" name="docid" id="docid" value="{{ $docid ?? '' }}">
-                    <div class="mb-3 row">
-                        <label for="docDateEnd" class="col-sm-2 col-form-label">Дата окончания действия документа:</label>
-                        <div class="col-sm-3">
-                            <input type="date"
-                                class="form-control form-control-sm  @error('docDateEnd') is-invalid @enderror"
-                                id="docDateEnd" name="docDateEnd" value="{{ old('docDateEnd') ?? $docData->docDateEnd }}">
-                            @error('docDateEnd')
-                                <div id="docDateEnd" class="invalid-feedback">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="col">
-                            <div class="mb-3">
-                                <button type="submit" class="btn btn-sm btn-success">Сохранить</button>
-                                <button type="button" class="btn btn-sm btn-warning" id="btnClearDate">Очистить</button>
-                            </div>
-                        </div>
-                    </div>
-                </form>
             </div>
         </div>
         <div class="table-responsive">
--- resources/views/admin/docs/parts/editableDetailData.blade.php
+++ resources/views/admin/docs/parts/editableDetailData.blade.php
@@ -2,6 +2,8 @@ editable
 <form method="POST" action="{{ route('osfrportal.admin.docs.saveeditable') }}" enctype="multipart/form-data">
     <input type="hidden" name="docid" id="docid" value="{{ $docid ?? '' }}">
     <div class="mb-3 row">
-        <button type="submit" class="btn btn-sm btn-success">Сохранить</button>
+        <div class="col">
+            <button type="submit" class="btn btn-sm btn-success">Сохранить</button>
+        </div>
     </div>
 </form>
