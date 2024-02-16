03762c24b75aba0a4bc1e0303c5fa072ba26889c
Paul Pleshkov <pleshkovpa@yandex.ru>
2024-02-16 22:24:46 +0300

    sc

--- src/Http/Controllers/Admin/SFRMainteranceAdminController.php
+++ src/Http/Controllers/Admin/SFRMainteranceAdminController.php
@@ -4,13 +4,22 @@ namespace Osfrportal\OsfrportalLaravel\Http\Controllers\Admin;
 
 use Osfrportal\OsfrportalLaravel\Data\SFRMsgStatusData;
 
+use Illuminate\Support\Facades\Redis;
+
 class SFRMainteranceAdminController extends Controller
 {
+    private $redisKey = 'mainterance:test';
+
     public function mainteranceIndex()
     {
         $test = SFRMsgStatusData::from(['error' => false, 'message' => 'Тестовое сообщение']);
-        dump($test);
+
         dump($test->toJson());
+        Redis::set($this->redisKey, $test->toJson());
+        if (Redis::exists($this->redisKey)) {
+            dump(Redis::get($this->redisKey));
+        }
+        Redis::del($this->redisKey);
         return view('osfrportal::admin.mainterance.index');
     }
 }
