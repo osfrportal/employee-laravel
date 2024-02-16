f0798bd35b01949df7df3e8f45b94a02b6b16045
Paul Pleshkov <pleshkovpa@yandex.ru>
2024-02-16 22:34:26 +0300

    sc

--- src/Http/Controllers/Admin/SFRMainteranceAdminController.php
+++ src/Http/Controllers/Admin/SFRMainteranceAdminController.php
@@ -8,16 +8,16 @@ use Illuminate\Support\Facades\Redis;
 
 class SFRMainteranceAdminController extends Controller
 {
-    private $redisKey = 'mainterance:test';
+    private $redisMKey = 'mainterance:test';
 
     public function mainteranceIndex()
     {
         $test = SFRMsgStatusData::from(['error' => false, 'message' => 'Тестовое сообщение']);
 
         dump($test->toJson());
-        Redis::set($this->redisKey, $test->toJson());
-        if (Redis::exists($this->redisKey)) {
-            dump(Redis::get($this->redisKey));
+        Redis::set($this->redisMKey, $test->toJson());
+        if (Redis::exists($this->redisMKey)) {
+            dump(Redis::get($this->redisMKey));
         }
         //Redis::del($this->redisKey);
         return view('osfrportal::admin.mainterance.index');
