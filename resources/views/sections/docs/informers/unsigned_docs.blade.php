a7b854d9e10b979f2c0d5a1a9b821857f90db0bd
osfrportal <pleshkovpa@48.sfr.gov.ru>
2024-02-17 11:57:25 +0300

    some changes

--- resources/views/sections/docs/informers/unsigned_docs.blade.php
+++ resources/views/sections/docs/informers/unsigned_docs.blade.php
@@ -1,6 +1,7 @@
     <div class="my-3 p-3 bg-body rounded shadow-sm text-center" style="max-width: 15rem;">
         <div class="card-header">Документов на ознакомление</div>
         <div class="card-body text-primary">
-            <a class="text-decoration-none" href="{{ route('osfrportal.docs.index') }}"><h2 class="bi bi-file-medical card-text"> {{ $docsUnsignedCount ?? '0' }} </h2></a>
+            <div class="card-text fs-4"><a class="text-decoration-none" href="{{ route('osfrportal.docs.index') }}"><i
+                        class="bi bi-file-medical"></i> {{ $docsUnsignedCount ?? '0' }} </a></div>
         </div>
     </div>
