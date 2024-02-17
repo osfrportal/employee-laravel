68d08d77063f16091d190ee9904bdb080efcc89c
osfrportal <pleshkovpa@48.sfr.gov.ru>
2024-02-17 11:55:27 +0300

    some changes

--- resources/views/sections/dashboard/informers/myip.blade.php
+++ resources/views/sections/dashboard/informers/myip.blade.php
@@ -1,6 +1,6 @@
     <div class="my-3 p-3 bg-body rounded shadow-sm text-center" style="max-width: 15rem;">
         <div class="card-header">Ваш IP</div>
         <div class="card-body text-primary">
-            <div class="card-text fs-5"><i class="ti ti-devices-pc"></i> {{ $myIp ?? '' }} </div>
+            <div class="card-text fs-4"><i class="ti ti-devices-pc"></i> {{ $myIp ?? '' }} </div>
         </div>
     </div>
