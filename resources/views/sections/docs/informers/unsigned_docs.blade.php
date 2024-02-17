c9130144d90f956c62c696e2c523af95de96c2ed
osfrportal <pleshkovpa@48.sfr.gov.ru>
2024-02-17 11:59:25 +0300

    some changes

--- resources/views/sections/docs/informers/unsigned_docs.blade.php
+++ resources/views/sections/docs/informers/unsigned_docs.blade.php
@@ -1,7 +1,9 @@
-    <div class="my-3 p-3 bg-body rounded shadow-sm text-center" style="max-width: 15rem;">
-        <div class="card-header">Документов на ознакомление</div>
-        <div class="card-body text-primary">
-            <div class="card-text fs-4"><a class="text-decoration-none" href="{{ route('osfrportal.docs.index') }}"><i
-                        class="bi bi-file-medical"></i> {{ $docsUnsignedCount ?? '0' }} </a></div>
+    @if ($docsUnsignedCount > 0)
+        <div class="my-3 p-3 bg-body rounded shadow-sm text-center" style="max-width: 15rem;">
+            <div class="card-header">Документов на ознакомление</div>
+            <div class="card-body text-primary">
+                <div class="card-text fs-4"><a class="text-decoration-none" href="{{ route('osfrportal.docs.index') }}"><i
+                            class="bi bi-file-medical"></i> {{ $docsUnsignedCount ?? '0' }} </a></div>
+            </div>
         </div>
-    </div>
+    @endif
