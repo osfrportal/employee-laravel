a6e7037ad73bd733265a9eb7667f336878a67ea8
Paul Pleshkov <pleshkovpa@yandex.ru>
2024-02-26 12:10:04 +0300

    sc

--- resources/views/livewire/admin/mainterance-messages.blade.php
+++ resources/views/livewire/admin/mainterance-messages.blade.php
@@ -1,18 +1,20 @@
 <div wire:poll.15s>
-    <div class="my-3 p-3 bg-body rounded shadow-sm text-center text-primary" style="max-width: 25rem;">
-        <div class="card-header fs-4">IMAP загрузка писем от 1С</div>
+    <div class="my-3 p-3 bg-body rounded shadow-sm text-center text-primary" style="max-width: 20rem;">
+        <div class="card-header fs-4">
+            @if ($imapMessage->canRunImports)
+                <i class="ti ti-file-check"></i>
+            @else
+                @if ($imapMessage->error)
+                    <i class="ti ti-file-x"></i>
+                @endif
+                @if ($imapMessage->tryAgain)
+                    <i class="ti ti-file-infinity"></i>
+                @endif
+            @endif
+            IMAP загрузка писем от 1С
+        </div>
         <div class="card-body">
             <div class="card-text fs-6">
-                @if ($imapMessage->canRunImports)
-                    <i class="ti ti-file-check"></i>
-                @else
-                    @if ($imapMessage->error)
-                        <i class="ti ti-file-x"></i>
-                    @endif
-                    @if ($imapMessage->tryAgain)
-                        <i class="ti ti-file-infinity"></i>
-                    @endif
-                @endif
                 {{ $imapMessage->message ?? '' }}
             </div>
         </div>
