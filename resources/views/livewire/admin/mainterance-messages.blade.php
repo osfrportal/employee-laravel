b6731eb18bcb3026bea724435d70e28d663c09bf
Paul Pleshkov <pleshkovpa@yandex.ru>
2024-02-26 12:04:48 +0300

    sc

--- resources/views/livewire/admin/mainterance-messages.blade.php
+++ resources/views/livewire/admin/mainterance-messages.blade.php
@@ -1,23 +1,19 @@
 <div wire:poll.15s>
     <div class="my-3 p-3 bg-body rounded shadow-sm text-center text-primary" style="max-width: 25rem;">
-        <div class="card-header">IMAP загрузка писем от 1С</div>
+        <div class="card-header fs-4">IMAP загрузка писем от 1С</div>
         <div class="card-body">
-            <div class="card-text">
-                <div class="fs-4">
-                    @if ($imapMessage->canRunImports)
-                        <i class="ti ti-file-check"></i>
-                    @else
-                        @if ($imapMessage->error)
-                            <i class="ti ti-file-x"></i>
-                        @endif
-                        @if ($imapMessage->tryAgain)
-                            <i class="ti ti-file-infinity"></i>
-                        @endif
+            <div class="card-text fs-6">
+                @if ($imapMessage->canRunImports)
+                    <i class="ti ti-file-check"></i>
+                @else
+                    @if ($imapMessage->error)
+                        <i class="ti ti-file-x"></i>
                     @endif
-                </div>
-                <div class="fs-6">
-                    {{ $imapMessage->message ?? '' }}
-                </div>
+                    @if ($imapMessage->tryAgain)
+                        <i class="ti ti-file-infinity"></i>
+                    @endif
+                @endif
+                {{ $imapMessage->message ?? '' }}
             </div>
         </div>
         <div class="card-footer text-muted">
