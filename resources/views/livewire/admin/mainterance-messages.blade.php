af0c1bd6e99bb4232d6619a51bfbbb195d154227
Paul Pleshkov <pleshkovpa@yandex.ru>
2024-02-26 12:12:43 +0300

    sc

--- resources/views/livewire/admin/mainterance-messages.blade.php
+++ resources/views/livewire/admin/mainterance-messages.blade.php
@@ -1,5 +1,5 @@
 <div wire:poll.15s>
-    <div class="my-3 p-3 bg-body rounded shadow-sm text-center text-primary" style="max-width: 20rem;">
+    <div class="my-3 p-3 bg-body rounded shadow-sm text-center text-primary" style="max-width: 25rem;">
         <div class="card-header fs-4">
             @if ($imapMessage->canRunImports)
                 <i class="ti ti-file-check"></i>
@@ -11,7 +11,7 @@
                     <i class="ti ti-file-infinity"></i>
                 @endif
             @endif
-            IMAP загрузка писем от 1С
+            Загрузка писем от 1С
         </div>
         <div class="card-body">
             <div class="card-text fs-6">
