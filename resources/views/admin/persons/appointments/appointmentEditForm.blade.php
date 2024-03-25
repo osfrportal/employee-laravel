65618049143305318230f6da2db98f079d54979f
Paul Pleshkov <pleshkovpa@yandex.ru>
2024-03-25 23:39:41 +0300

    sc

--- resources/views/admin/persons/appointments/appointmentEditForm.blade.php
+++ resources/views/admin/persons/appointments/appointmentEditForm.blade.php
@@ -1,4 +1,34 @@
 @extends('osfrportal::layout')
 @section('content')
     @dump($appointment)
+    <form method="POST" action="{{ route('osfrportal.admin.persons.appointments.detail', $appointment->aid) }}">
+        <input type="hidden" id="aid" name="aid" value="{{ $appointment->ai }}">
+        <div class="card mb-4">
+            <div class="card-header">Редактирование должности</div>
+            <div class="card-body">
+                <div class="mb-3">
+                    <label class="mb-1" for="aname">Наименование</label>
+                    <input class="form-control form-control-sm" id="aname" type="text"
+                        value="{{ $appointment->aname ?? '' }}" disabled readonly>
+                    <div class="font-italic text-muted mb-4">
+                        Заполняется автоматически из выгрузки 1С
+                    </div>
+                </div>
+                <div class="mb-3">
+                    <label class="mb-1" for="asortorder">Порядок сортировки:</label>
+                    <input class="form-control form-control-sm @error('asortorder') is-invalid @enderror" id="asortorder"
+                        name="asortorder" type="text" placeholder="Введите число"
+                        value="{{ old('asortorder') ?? ($appointment->asortorder ?? '') }}">
+                    @error('asortorder')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
+                    <div class="font-italic text-muted mb-4">
+                        Чем меньше число, тем выше в списке
+                    </div>
+                </div>
+                <button class="btn btn-primary btn-submit" type="submit"
+                    @if ($appointment->sfrpersons_count > 0) disabled @endif>Сохранить</button>
+            </div>
+        </div>
+    </form>
 @endsection
