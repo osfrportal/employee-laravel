1fc1b70bc828d9df3042dd6d3d22a4e03f04b844
Paul Pleshkov <pleshkovpa@yandex.ru>
2024-03-26 21:31:03 +0300

    sc

--- resources/views/admin/docs/reports/reports_index.blade.php
+++ resources/views/admin/docs/reports/reports_index.blade.php
@@ -1,15 +1,5 @@
 @extends('osfrportal::layout')
-@section('title2')
-    <div class="pt-0">
-        <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="#">Администрирование</a></li>
-                <li class="breadcrumb-item">Управление документами</li>
-                <li class="breadcrumb-item active">Отчеты</li>
-            </ol>
-        </nav>
-    </div>
-@endsection
 @section('content')
-    <a href="{{ route('osfrportal.admin.docs.reports.byunits') }}" title="Отчет в разрезе подразделений">Ознакомление по подразделениям</a>
+    <a href="{{ route('osfrportal.admin.docs.reports.byunits') }}" title="Отчет в разрезе подразделений">Ознакомление по
+        подразделениям</a>
 @endsection
