8136ac05a14c886ba64ffb38db1343197ebb10f6
Paul Pleshkov <pleshkovpa@yandex.ru>
2024-04-08 22:57:29 +0300

    sc

--- src/Imports/SFRRcaEmployeeImport.php
+++ src/Imports/SFRRcaEmployeeImport.php
@@ -2,18 +2,29 @@
 namespace Osfrportal\OsfrportalLaravel\Imports;
 
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
 
 class SFRRcaEmployeeImport
 {
     public function import($filename, $storage)
     {
+        $worked = 0;
+        $fired = 0;
         if (Storage::disk($storage)->exists($filename)) {
             $xmlString = Storage::disk($storage)->get($filename);
             $xmlData = simplexml_load_string($xmlString);
             $persons = $xmlData->xpath('//Persons/Person');
             foreach ($persons as $person) {
-                dump($person);
+                if (Str::is($person->state[0], 'Работает')) {
+                    $worked++;
+                }
+                if (Str::is($person->state[0], 'Уволен')) {
+                    $fired++;
+                }
+                //dump($person);
             }
+            dump($worked, $fired);
         }
     }
 }
