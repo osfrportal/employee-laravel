22f8b19f4da526e2e1dac283a8ce1c298ccb6837
Paul Pleshkov <pleshkovpa@yandex.ru>
2024-04-08 22:51:15 +0300

    sc

--- src/Imports/SFRRcaEmployeeImport.php
+++ src/Imports/SFRRcaEmployeeImport.php
@@ -10,7 +10,7 @@ class SFRRcaEmployeeImport
         if (Storage::disk($storage)->exists($filename)) {
             $xmlString = Storage::disk($storage)->get($filename);
             $xmlData = simplexml_load_string($xmlString);
-            $units = $xmlData->xpath('//Persons/Person');
+            $persons = $xmlData->xpath('//Persons/Person');
             foreach ($persons as $person) {
                 dump($person);
             }
