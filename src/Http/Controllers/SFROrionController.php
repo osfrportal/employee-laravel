38e1db5c16cdb1e3d8bd5f43f27b920128a49b90
osfrportal <pleshkovpa@48.sfr.gov.ru>
2024-04-17 19:17:41 +0300

    some changes

--- src/Http/Controllers/SFROrionController.php
+++ src/Http/Controllers/SFROrionController.php
@@ -71,7 +71,7 @@ class SFROrionController extends Controller
             'LastName' => 'LastName',
             'FirstName' => 'FirstName',
             'MiddleName' => 'MiddleName',
-            //'BirthDate' => '1899-12-30T00:00:00.000+03:00',
+            'ExternalId' => '',
             'CompanyId' => -1,
             'DepartmentId' => -1,
             'PositionId' => -1,
@@ -79,10 +79,6 @@ class SFROrionController extends Controller
             'AccessLevelId' => 0,
             'Status' => 5,
             'Itn' => '123123123',
-            //'DocumentIssueDate' => '1899-12-30T00:00:00.000+03:00',
-            //'DocumentEndingDate' => '1899-12-30T00:00:00.000+03:00',
-            //'ArchivingTimeStamp' => '1899-12-30T00:00:00.000+03:00',
-            //'ChangeTime' => Carbon::now()->format('Y-m-d\TH:i:s.000O'),
         ]);
         $pDataArr = $personData->toArray();
         dump($pDataArr);
