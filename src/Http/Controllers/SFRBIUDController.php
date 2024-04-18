ef27da39de1d0124df291f6049742a3c702eae25
osfrportal <pleshkovpa@48.sfr.gov.ru>
2024-04-18 20:10:55 +0300

    some changes

--- src/Http/Controllers/SFRBIUDController.php
+++ src/Http/Controllers/SFRBIUDController.php
@@ -102,7 +102,7 @@ class SFRBIUDController extends Controller
         $systemsArray = Str::of($systems)->explode(',');
         foreach ($systemsArray as $system) {
             foreach ($this->getRolesBySystem(trim($system)) as $role) {
-                $r = new SFRInfosystemRoleData(roleData: $role);
+                $r = new SFRInfosystemRoleData(roleName: $role['remplusname'], roleData: $role);
                 $rolesCollection->push($r);
             }
         }
