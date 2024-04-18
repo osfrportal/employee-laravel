c9481e3afbe4c19b3a8fa7e1f999991c9c058940
osfrportal <pleshkovpa@48.sfr.gov.ru>
2024-04-18 20:18:20 +0300

    some changes

--- src/Data/SFRInfosystemRoleData.php
+++ src/Data/SFRInfosystemRoleData.php
@@ -11,16 +11,16 @@ class SFRInfosystemRoleData extends Data
 {
     /**
      * Конструктор DTO Роль информационной системы
-     * @param string|Optional $roleID Идентификатор роли в формате uuid
      * @param string $roleName Имя роли
      * @param array $roleData Данные роли
      * @param string $roleDescription Описание роли/комментарий
+     * @param string|Optional $roleID Идентификатор роли в формате uuid
      */
     public function __construct(
-        public string|Optional $roleID,
         public string $roleName = '',
         public ?array $roleData = [],
         public ?string $roleDescription = '',
+        public string|Optional $roleID,
     ) {
     }
 }
