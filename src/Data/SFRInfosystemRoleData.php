3a18dda4b2b482fc57be7ec86f4104b25389e9e2
osfrportal <pleshkovpa@48.sfr.gov.ru>
2024-04-18 20:16:24 +0300

    some changes

--- src/Data/SFRInfosystemRoleData.php
+++ src/Data/SFRInfosystemRoleData.php
@@ -3,19 +3,21 @@
 namespace Osfrportal\OsfrportalLaravel\Data;
 
 use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Optional;
+
 use Carbon\Carbon;
 
 class SFRInfosystemRoleData extends Data
 {
     /**
      * Конструктор DTO Роль информационной системы
-     * @param string $roleID Идентификатор роли в формате uuid
+     * @param string|Optional $roleID Идентификатор роли в формате uuid
      * @param string $roleName Имя роли
      * @param array $roleData Данные роли
      * @param string $roleDescription Описание роли/комментарий
      */
     public function __construct(
-        public string $roleID = '',
+        public string|Optional $roleID,
         public string $roleName = '',
         public ?array $roleData = [],
         public ?string $roleDescription = '',
