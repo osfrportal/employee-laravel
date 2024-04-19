f7ed19fb680035de03e45c63f299c93102780d44
osfrportal <pleshkovpa@48.sfr.gov.ru>
2024-04-19 15:33:36 +0300

    some changes

--- resources/views/admin/infosystems/infosystemDetail.blade.php
+++ resources/views/admin/infosystems/infosystemDetail.blade.php
@@ -8,18 +8,33 @@
                 <div class="col">{{ $infoSystemModel->isys_name }}</div>
             </div>
             <div class="row">
-                <div class="col">
-                    <div class="card m-4 m-xl-0">
+                <div class="col mt-4">
+                    <div class="card mb-4 mb-xl-0">
                         <div class="card-header">Данные ИС</div>
                         <div class="card-body">
                             <div class="row">
                                 <div class="col">Синхронизация с ИС</div>
-                                <div class="col">{{ (bool) $infoSystemModel->isys_data->syncWithIS }}</div>
+                                <div class="col">{{ $infoSystemModel->isys_data->syncWithIS }}</div>
                             </div>
-                            <div class="row">
-                                <div class="col">controllerNameSync</div>
-                                <div class="col">@dump($infoSystemModel->isys_data)</div>
-                            </div>
+                            @if ($infoSystemModel->isys_data->syncWithIS === true)
+                                <div class="row">
+                                    <div class="col">controllerNameSync</div>
+                                    <div class="col">@dump($infoSystemModel->isys_data)</div>
+                                </div>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="card mb-4 mb-xl-0">
+                    <div class="card-header">Полномочия информационной системы</div>
+                    <div class="card-body">
+                        <div class="row">
+                            1
+                        </div>
+                        <div class="row">
+                            2
                         </div>
                     </div>
                 </div>
