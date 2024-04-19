8f0032aed9909416a029ab62a45e68e61422438d
osfrportal <pleshkovpa@48.sfr.gov.ru>
2024-04-19 15:26:50 +0300

    some changes

--- resources/views/admin/infosystems/infosystemDetail.blade.php
+++ resources/views/admin/infosystems/infosystemDetail.blade.php
@@ -9,12 +9,12 @@
             </div>
             <div class="row">
                 <div class="col">
-                    <div class="card mb-4 mb-xl-0">
+                    <div class="card m-4 m-xl-0">
                         <div class="card-header">Данные ИС</div>
                         <div class="card-body">
                             <div class="row">
                                 <div class="col">Синхронизация с ИС</div>
-                                <div class="col">{{ $infoSystemModel->isys_data->syncWithIS }}</div>
+                                <div class="col">{{ (bool) $infoSystemModel->isys_data->syncWithIS }}</div>
                             </div>
                             <div class="row">
                                 <div class="col">controllerNameSync</div>
