22632601fc3980d97b11beecc2beb7f22d378568
osfrportal <pleshkovpa@48.sfr.gov.ru>
2024-04-19 12:05:39 +0300

    some changes

--- resources/views/admin/infosystems/infosystemDetail.blade.php
+++ resources/views/admin/infosystems/infosystemDetail.blade.php
@@ -1,7 +1,7 @@
 @extends('osfrportal::layout')
 @section('content')
     <div class="card mb-4 mb-xl-0">
-        <div class="card-header">Информационная система</div>
+        <div class="card-header">Карточка информационной системы</div>
         <div class="card-body">
             <div class="row">
                 <div class="col">Наименование ИС</div>
@@ -14,11 +14,11 @@
                         <div class="card-body">
                             <div class="row">
                                 <div class="col">Синхронизация с ИС</div>
-                                <div class="col">{{ $infoSystemModel->infoSystemModel->syncWithIS }}</div>
+                                <div class="col">{{ $infoSystemModel->isys_data->syncWithIS }}</div>
                             </div>
                             <div class="row">
                                 <div class="col">controllerNameSync</div>
-                                <div class="col">{{ $infoSystemModel->infoSystemModel->controllerNameSync ?? '' }}</div>
+                                <div class="col">{{ $infoSystemModel->isys_data->controllerNameSync ?? '' }}</div>
                             </div>
                         </div>
                     </div>
